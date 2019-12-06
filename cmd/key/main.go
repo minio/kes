c17971d5848e5c67e0e5c5793212b2919bbc15f7
Andreas Auernhammer <aead@mail.de>
2019-12-06 15:05:00 +0100

    move util functions into main.go

--- cmd/key/main.go
+++ cmd/key/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"io"
 	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const usage = `usage: %s <command>
@@ -64,3 +67,25 @@ func failf(w io.Writer, format string, args ...interface{}) {
 	fmt.Fprintf(w, format, args...)
 	os.Exit(1)
 }
+
+func serverAddr() string {
+	if addr, ok := os.LookupEnv("KEY_SERVER"); ok {
+		return addr
+	}
+	return "https://127.0.0.1:7373"
+}
+
+func loadClientCertificates() []tls.Certificate {
+	certPath := os.Getenv("KEY_CLIENT_TLS_CERT_FILE")
+	keyPath := os.Getenv("KEY_CLIENT_TLS_KEY_FILE")
+	if certPath != "" || keyPath != "" {
+		cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+		if err != nil {
+			failf(os.Stderr, "Cannot load TLS key or cert for client auth: %s", err.Error())
+		}
+		return []tls.Certificate{cert}
+	}
+	return nil
+}
+
+func isTerm(f *os.File) bool { return terminal.IsTerminal(int(f.Fd())) }
