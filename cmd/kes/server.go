minio/kes

commit 817a824889ffc9a8b22145897e069cfeda5643e0

implement encrypted secrets using a KMS
This commit implements encrypted secrets.
Before, KES stored all secrets as plaintext at the
backend secret store. Therefore, KES assumed/required
that the secret store is a secure/trusted component.

While this assumption may be true for secret stores like
Vault or AWS SecretsManager it is not true in the general
case. Further, even if the secret store is considered trusted
it may be desirable to separate the trust between the secret
storage provider and another party.

Therefore, a KES server may want to encrypt secrets before
storing them at the secret store. In general, KES encrypts
secrets with an (external) KMS - like Vault, AWS-KMS or
Azure Vault. Therefore, KES sends a secret to the KMS before
writing it to the secret store. However, it doesn't send the
secret as plaintext to the KMS since that would reveal the
secret to the operator of the KMS / anyone who can inspect
KMS operations.

Instead, it generates a random (local) cryptographic key
when creating a secret and encrypts the secret with that key
*before* sending the secret to the KMS. So, the KMS only
sees ciphertexts (`Enc(local_key, secret)`). The local key
is then embedded into the value written to the secret store.

This construction ensures that neither the secret store provider
nor the KMS provider can decrypt an encrypted secret on their own.
So, it is possible to leverage e.g. a cloud KMS provider (AWS, Google,
MS, ...) to encrypt secrets on some local storage without sharing
these secrets with the cloud provider.

***

Therefore, this commit changes the architecture of the exiting code
quite a bit:

There is now a local store (`secret.Store`) and a remote store
(`secret.Remote`). The `secret.Store` implements en/decryption
(via the `secret.KMS` interface) and is responsible for caching
secrets.
The `secret.Remote` interface defines what operations a secret
store has to provide and must be implemented by all secret store
implementations.

cmd/kes/server.go
@@ -172,7 +172,25 @@ func server(args []string) error {
 		}
 	}
 
-	var store secret.Store
+	var store = &secret.Store{}
+	switch {
+	case config.KMS.AWS.Addr != "":
+		kms := &aws.KMS{
+			Addr:   config.KMS.AWS.Addr,
+			Region: config.KMS.AWS.Region,
+			Login: aws.Credentials{
+				AccessKey:    config.KMS.AWS.Login.AccessKey,
+				SecretKey:    config.KMS.AWS.Login.SecretKey,
+				SessionToken: config.KMS.AWS.Login.SessionToken,
+			},
+		}
+		if err = kms.Authenticate(); err != nil {
+			return fmt.Errorf("Failed to connect to AWS-KMS: %v", err)
+		}
+		store.KMS = kms
+		store.Key = config.KMS.AWS.Key
+	}
+
 	switch {
 	case config.KeyStore.Fs.Dir != "":
 		f, err := os.Stat(config.KeyStore.Fs.Dir)
@@ -187,14 +205,12 @@ func server(args []string) error {
 				return fmt.Errorf("Failed to create directory %s: %v", config.KeyStore.Fs.Dir, err)
 			}
 		}
-		store = &fs.KeyStore{
-			Dir:                    config.KeyStore.Fs.Dir,
-			CacheExpireAfter:       config.Cache.Expiry.All,
-			CacheExpireUnusedAfter: config.Cache.Expiry.Unused,
-			ErrorLog:               errorLog.Log(),
+		store.Remote = &fs.Store{
+			Dir:      config.KeyStore.Fs.Dir,
+			ErrorLog: errorLog.Log(),
 		}
 	case config.KeyStore.Vault.Addr != "":
-		vaultStore := &vault.KeyStore{
+		vaultStore := &vault.Store{
 			Addr:      config.KeyStore.Vault.Addr,
 			Location:  config.KeyStore.Vault.Name,
 			Namespace: config.KeyStore.Vault.Namespace,
@@ -203,26 +219,22 @@ func server(args []string) error {
 				Secret: config.KeyStore.Vault.AppRole.Secret,
 				Retry:  config.KeyStore.Vault.AppRole.Retry,
 			},
-			CacheExpireAfter:       config.Cache.Expiry.All,
-			CacheExpireUnusedAfter: config.Cache.Expiry.Unused,
-			StatusPingAfter:        config.KeyStore.Vault.Status.Ping,
-			ErrorLog:               errorLog.Log(),
-			ClientKeyPath:          config.KeyStore.Vault.TLS.KeyPath,
-			ClientCertPath:         config.KeyStore.Vault.TLS.CertPath,
-			CAPath:                 config.KeyStore.Vault.TLS.CAPath,
+			StatusPingAfter: config.KeyStore.Vault.Status.Ping,
+			ErrorLog:        errorLog.Log(),
+			ClientKeyPath:   config.KeyStore.Vault.TLS.KeyPath,
+			ClientCertPath:  config.KeyStore.Vault.TLS.CertPath,
+			CAPath:          config.KeyStore.Vault.TLS.CAPath,
 		}
 		if err := vaultStore.Authenticate(context.Background()); err != nil {
 			return fmt.Errorf("Failed to connect to Vault: %v", err)
 		}
-		store = vaultStore
+		store.Remote = vaultStore
 	case config.KeyStore.Aws.SecretsManager.Addr != "":
 		awsStore := &aws.SecretsManager{
-			Addr:                   config.KeyStore.Aws.SecretsManager.Addr,
-			Region:                 config.KeyStore.Aws.SecretsManager.Region,
-			KmsKeyID:               config.KeyStore.Aws.SecretsManager.KmsKeyID,
-			CacheExpireAfter:       config.Cache.Expiry.All,
-			CacheExpireUnusedAfter: config.Cache.Expiry.Unused,
-			ErrorLog:               errorLog.Log(),
+			Addr:     config.KeyStore.Aws.SecretsManager.Addr,
+			Region:   config.KeyStore.Aws.SecretsManager.Region,
+			KMSKeyID: config.KeyStore.Aws.SecretsManager.KmsKeyID,
+			ErrorLog: errorLog.Log(),
 			Login: aws.Credentials{
 				AccessKey:    config.KeyStore.Aws.SecretsManager.Login.AccessKey,
 				SecretKey:    config.KeyStore.Aws.SecretsManager.Login.SecretKey,
@@ -232,14 +244,11 @@ func server(args []string) error {
 		if err := awsStore.Authenticate(); err != nil {
 			return fmt.Errorf("Failed to connect to AWS Secrets Manager: %v", err)
 		}
-		store = awsStore
+		store.Remote = awsStore
 	default:
-		store = &mem.KeyStore{
-			CacheExpireAfter:       config.Cache.Expiry.All,
-			CacheExpireUnusedAfter: config.Cache.Expiry.Unused,
-			ErrorLog:               errorLog.Log(),
-		}
+		store.Remote = &mem.Store{}
 	}
+	store.StartGC(context.Background(), config.Cache.Expiry.All, config.Cache.Expiry.Unused)
 
 	var proxy *auth.TLSProxy
 	if len(config.TLS.Proxy.Identities) != 0 {

cmd/kes/config.go
@@ -51,6 +51,21 @@ type serverConfig struct {
 		} `toml:"audit" yaml:"audit"`
 	} `toml:"log" yaml:"log"`
 
+	KMS struct {
+		AWS struct {
+			Addr   string `toml:"address" yaml:"address"`
+			Region string `toml:"region" yaml:"region"`
+
+			Key string `toml:"key" yaml:"key"`
+
+			Login struct {
+				AccessKey    string `toml:"access_key" yaml:"access_key"`
+				SecretKey    string `toml:"secret_key" yaml:"secret_key"`
+				SessionToken string `toml:"session_token" yaml:"session_token"`
+			} `toml:"credentials" yaml:"credentials"`
+		} `toml:"aws" yaml:"aws"`
+	} `toml:"kms" yaml:"kms"`
+
 	KeyStore struct {
 		Fs struct {
 			Dir string `toml:"path" yaml:"path"`

error.go
@@ -14,6 +14,7 @@ import (
 var (
 	ErrKeyNotFound Error = NewError(http.StatusNotFound, "key does not exist")
 	ErrKeyExists   Error = NewError(http.StatusBadRequest, "key does already exist")
+	ErrKeySealed   Error = NewError(http.StatusForbidden, "key is sealed")
 	ErrNotAllowed  Error = NewError(http.StatusForbidden, "prohibited by policy")
 )
 
@@ -34,7 +35,7 @@ var (
 //       case ErrNotAllowed:
 //          // We don't have the permission to create this key.
 //       default:
-//          // Something else when wrong.
+//          // Something else went wrong.
 //   }
 type Error struct {
 	code    int

internal/aws/kms.go
@@ -1,0 +1,163 @@
+// Copyright 2020 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package aws
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	awskms "github.com/aws/aws-sdk-go/service/kms"
+	"github.com/minio/kes"
+	"github.com/minio/kes/internal/secret"
+)
+
+// KMS is an AWS-KMS client that implements
+// the secret.KMS interface.
+//
+// It can be used to encrypt secrets before
+// storing them at a key store resp. decrypt
+// them after fetching them from such a store.
+type KMS struct {
+	// Addr is the HTTP address of the AWS KMS.
+	// In general, the address has the following
+	// form:
+	//  kms.<region>.amazonaws.com
+	Addr string
+
+	// Region is the AWS region. Even though the Addr
+	// endpoint contains that information already, this
+	// field is mandatory.
+	Region string
+
+	// Login contains the AWS credentials (access/secret key).
+	Login Credentials
+
+	// ErrorLog specifies an optional logger for errors
+	// when files cannot be opened, deleted or contain
+	// invalid content.
+	// If nil, logging is done via the log package's
+	// standard logger.
+	ErrorLog *log.Logger
+
+	client *awskms.KMS
+}
+
+var _ secret.KMS = (*KMS)(nil)
+
+// Authenticate tries to establish a connection to
+// the AWS KMS using the login credentials.
+func (kms *KMS) Authenticate() error {
+	credentials := credentials.NewStaticCredentials(
+		kms.Login.AccessKey,
+		kms.Login.SecretKey,
+		kms.Login.SessionToken,
+	)
+	if kms.Login.AccessKey == "" && kms.Login.SecretKey == "" && kms.Login.SessionToken == "" {
+		// If all login credentials (access key, secret key and session token) are empty
+		// we pass no (not empty) credentials to the AWS SDK. The SDK will try to fetch
+		// the credentials from:
+		//  - Environment Variables
+		//  - Shared Credentials file
+		//  - EC2 Instance Metadata
+		// In particular, when running a kes server on an EC2 instance, the SDK will
+		// automatically fetch the temp. credentials from the EC2 metadata service.
+		// See: AWS IAM roles for EC2 instances.
+		credentials = nil
+	}
+
+	session, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Endpoint:    aws.String(kms.Addr),
+			Region:      aws.String(kms.Region),
+			Credentials: credentials,
+		},
+		SharedConfigState: session.SharedConfigDisable,
+	})
+	if err != nil {
+		return err
+	}
+	kms.client = awskms.New(session)
+	return nil
+}
+
+// Encrypt tries to encrypt the given plaintext with the specified
+// CMK at the AWS-KMS instance. It returns the encrypted plaintext
+// as ciphertext.
+func (kms *KMS) Encrypt(key string, plaintext []byte) ([]byte, error) {
+	ciphertext, err := kms.client.Encrypt(&awskms.EncryptInput{
+		KeyId:     aws.String(key),
+		Plaintext: plaintext,
+	})
+	if err != nil {
+		if err, ok := err.(awserr.Error); ok {
+			switch err.Code() {
+			case awskms.ErrCodeNotFoundException:
+				kms.logf("aws: the CMK '%s' does not exist: %v", key, err)
+			case awskms.ErrCodeDisabledException:
+				kms.logf("aws: the CMK '%s' is disabled: %v", key, err)
+			case awskms.ErrCodeKeyUnavailableException:
+				kms.logf("aws: the CMK '%s' is not available: %v", key, err)
+			case awskms.ErrCodeInvalidKeyUsageException:
+				kms.logf("aws: the CMK '%s' cannot be used for encryption: %v", key, err)
+			case awskms.ErrCodeInvalidStateException:
+				kms.logf("aws: the CMK '%s' is in an invalid state: %v", key, err)
+			default:
+				kms.logf("aws: %v", err)
+			}
+		} else {
+			kms.logf("aws: %v", err)
+		}
+		return nil, kes.NewError(http.StatusInternalServerError, "cannot encrypt key")
+	}
+	return ciphertext.CiphertextBlob, nil
+}
+
+// Decrypt tries to decrypt the given ciphertext with the the given key
+// using the AWS-KMS. It returns the decrypted ciphertexts as plaintext
+// on success.
+func (kms *KMS) Decrypt(key string, ciphertext []byte) ([]byte, error) {
+	plaintext, err := kms.client.Decrypt(&awskms.DecryptInput{
+		KeyId:          aws.String(key),
+		CiphertextBlob: ciphertext,
+	})
+	if err != nil {
+		if err, ok := err.(awserr.Error); ok {
+			switch err.Code() {
+			case awskms.ErrCodeNotFoundException:
+				kms.logf("aws: the CMK '%s' does not exist", key)
+			case awskms.ErrCodeDisabledException:
+				kms.logf("aws: the CMK '%s' is disabled", key)
+			case awskms.ErrCodeInvalidCiphertextException:
+				kms.logf("aws: secret is not authentic: %v", err)
+			case awskms.ErrCodeIncorrectKeyException:
+				kms.logf("aws: secret was not encrypted with '%s'", key)
+			case awskms.ErrCodeKeyUnavailableException:
+				kms.logf("aws: the CMK '%s' is not available", key)
+			case awskms.ErrCodeInvalidKeyUsageException:
+				kms.logf("aws: the CMK '%s' cannot be used for decryption", key)
+			case awskms.ErrCodeInvalidStateException:
+				kms.logf("aws: the CMK '%s' is in an invalid state", key)
+			default:
+				kms.logf("aws: %v", err)
+			}
+		} else {
+			kms.logf("aws: %v", err)
+		}
+		return nil, kes.ErrKeySealed
+	}
+	return plaintext.Plaintext, nil
+}
+
+func (kms *KMS) logf(format string, v ...interface{}) {
+	if kms.ErrorLog == nil {
+		log.Printf(format, v...)
+	} else {
+		kms.ErrorLog.Printf(format, v...)
+	}
+}

internal/aws/secrets-manager.go
@@ -5,13 +5,10 @@
 package aws
 
 import (
-	"bytes"
-	"context"
 	"errors"
 	"fmt"
 	"log"
-	"sync/atomic"
-	"time"
+	"net/http"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -19,7 +16,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/minio/kes"
-	"github.com/minio/kes/internal/cache"
 	"github.com/minio/kes/internal/secret"
 )
 
@@ -31,39 +27,29 @@ type Credentials struct {
 	SessionToken string // The AWS session token
 }
 
-// SecretsManager is a secret key store that
-// saves/fetches secret keys on/from the AWS
+// SecretsManager is a  key-value store that
+// saves/fetches values as secrets  on/from the AWS
 // Secrets Manager.
 // See: https://aws.amazon.com/secrets-manager
 type SecretsManager struct {
 	// Addr is the HTTP address of the AWS Secret
-	// Manager. In general, you want to AWS directly.
-	// Therefore, use an address of the following
-	// form: secretsmanager.<region>.amazonaws.com
+	// Manager. In general, the address has the
+	// following form:
+	//  secretsmanager.<region>.amazonaws.com
 	Addr string
+
 	// Region is the AWS region. Even though the Addr
 	// endpoint contains that information already, this
 	// field is mandatory.
 	Region string
-	// The AWS-KMS key ID specifying the AWS-KMS key
-	// that is used to encrypt (and decrypt) the
-	// secret values stored at AWS Secrets Manager.
-	KmsKeyID string
+
+	// The KMSKeyID is the AWS-KMS key ID specifying the
+	// AWS-KMS key that is used to encrypt (and decrypt) the
+	// values stored at AWS Secrets Manager.
+	KMSKeyID string
 
 	// Login contains the AWS credentials (access/secret key).
 	Login Credentials
-
-	// CacheExpireAfter is the duration after which
-	// cache entries expire such that they have to
-	// be loaded from the backend storage again.
-	CacheExpireAfter time.Duration
-
-	// CacheExpireUnusedAfter is the duration after
-	// which not recently used cache entries expire
-	// such that they have to be loaded from the
-	// backend storage again.
-	// Not recently is defined as: CacheExpireUnusedAfter / 2
-	CacheExpireUnusedAfter time.Duration
 
 	// ErrorLog specifies an optional logger for errors
 	// when files cannot be opened, deleted or contain
@@ -72,76 +58,68 @@ type SecretsManager struct {
 	// standard logger.
 	ErrorLog *log.Logger
 
-	cache  cache.Cache
 	client *secretsmanager.SecretsManager
-	once   uint32
 }
 
-// Create adds the given secret key to the store if and only
-// if no entry for name exists. If an entry already exists
+var _ secret.Remote = (*SecretsManager)(nil)
+
+// Create stores the given key-value pair at the AWS SecretsManager
+// if and only if it doesn't exists. If such an entry already exists
 // it returns kes.ErrKeyExists.
 //
-// In particular, Create creates a new entry on AWS Secrets
-// Manager with the given name containing the secret.
-func (store *SecretsManager) Create(name string, secret secret.Secret) error {
-	if store.client == nil {
-		store.log(errNoConnection)
+// If the SecretsManager.KMSKeyID is set AWS will use this key ID to
+// encrypt the values. Otherwise, AWS will use the default key ID for
+// encrypting secrets at the AWS SecretsManager.
+func (s *SecretsManager) Create(key, value string) error {
+	if s.client == nil {
+		s.log(errNoConnection)
 		return errNoConnection
-	}
-	store.initialize()
-	if _, ok := store.cache.Get(name); ok {
-		return kes.ErrKeyExists
 	}
 
 	createOpt := secretsmanager.CreateSecretInput{
-		Name:         aws.String(name),
-		SecretString: aws.String(secret.String()),
+		Name:         aws.String(key),
+		SecretString: aws.String(value),
 	}
-	if store.KmsKeyID != "" {
-		createOpt.KmsKeyId = aws.String(store.KmsKeyID)
+	if s.KMSKeyID != "" {
+		createOpt.KmsKeyId = aws.String(s.KMSKeyID)
 	}
-	if _, err := store.client.CreateSecret(&createOpt); err != nil {
+	if _, err := s.client.CreateSecret(&createOpt); err != nil {
 		if err, ok := err.(awserr.Error); ok {
 			switch err.Code() {
 			case secretsmanager.ErrCodeResourceExistsException:
 				return kes.ErrKeyExists
 			}
 		}
-		err = fmt.Errorf("aws: failed to create secret '%s': %v", name, err)
-		store.log(err)
+		err = fmt.Errorf("aws: failed to create '%s': %v", key, err)
+		s.log(err)
 		return err
 	}
-	store.cache.Set(name, secret)
 	return nil
 }
 
-// Get returns the secret key associated with the given name.
-// If no entry for name exists, Get returns kes.ErrKeyNotFound.
-//
-// In particular, Get reads the secret key from the corresponding
-// entry at AWS Secrets Manager.
-func (store *SecretsManager) Get(name string) (secret.Secret, error) {
-	if store.client == nil {
-		store.log(errNoConnection)
-		return secret.Secret{}, errNoConnection
-	}
-	store.initialize()
-	if secret, ok := store.cache.Get(name); ok {
-		return secret, nil
+// Get returns the value associated with the given key.
+// If no entry for key exists, it returns kes.ErrKeyNotFound.
+func (s *SecretsManager) Get(key string) (string, error) {
+	if s.client == nil {
+		s.log(errNoConnection)
+		return "", errNoConnection
 	}
 
-	response, err := store.client.GetSecretValue(&secretsmanager.GetSecretValueInput{
-		SecretId: aws.String(name),
+	response, err := s.client.GetSecretValue(&secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(key),
 	})
 	if err != nil {
 		if err, ok := err.(awserr.Error); ok {
-			if err.Code() == secretsmanager.ErrCodeResourceNotFoundException {
-				return secret.Secret{}, kes.ErrKeyNotFound
+			switch err.Code() {
+			case secretsmanager.ErrCodeDecryptionFailure:
+				return "", kes.NewError(http.StatusForbidden, fmt.Sprintf("aws: cannot access '%s': %v", key, err))
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				return "", kes.ErrKeyNotFound
 			}
 		}
-		err = fmt.Errorf("aws: failed to read secret '%s': %v", name, err)
-		store.log(err)
-		return secret.Secret{}, err
+		err = fmt.Errorf("aws: failed to read '%s': %v", key, err)
+		s.log(err)
+		return "", err
 	}
 
 	// AWS has two different ways to store a secret. Either as
@@ -151,34 +129,22 @@ func (store *SecretsManager) Get(name string) (secret.Secret, error) {
 	// However, AWS demands and specifies that only one is present -
 	// either "SecretString" or "SecretBinary" - we can check which
 	// one is present and safely assume that the other one isn't.
-	var secret secret.Secret
 	if response.SecretString != nil {
-		if err = secret.ParseString(*response.SecretString); err != nil {
-			store.logf("aws: failed to read secret '%s': %v", name, err)
-			return secret, err
-		}
-	} else {
-		if _, err = secret.ReadFrom(bytes.NewReader(response.SecretBinary)); err != nil {
-			store.logf("aws: failed to read secret '%s': %v", name, err)
-			return secret, err
-		}
+		return *response.SecretString, nil
 	}
-	secret, _ = store.cache.Add(name, secret)
-	return secret, nil
+	return string(response.SecretBinary), nil
 }
 
-// Delete removes a the secret key with the given name
-// from the key store and deletes the corresponding AWS
-// Secrets Manager entry, if it exists.
-func (store *SecretsManager) Delete(name string) error {
-	if store.client == nil {
-		store.log(errNoConnection)
+// Delete removes the key-value pair from the AWS SecretsManager, if
+// it exists.
+func (s *SecretsManager) Delete(key string) error {
+	if s.client == nil {
+		s.log(errNoConnection)
 		return errNoConnection
 	}
-	store.cache.Delete(name)
 
-	_, err := store.client.DeleteSecret(&secretsmanager.DeleteSecretInput{
-		SecretId:                   aws.String(name),
+	_, err := s.client.DeleteSecret(&secretsmanager.DeleteSecretInput{
+		SecretId:                   aws.String(key),
 		ForceDeleteWithoutRecovery: aws.Bool(true),
 	})
 	if err != nil {
@@ -187,8 +153,8 @@ func (store *SecretsManager) Delete(name string) error {
 				return nil
 			}
 		}
-		err = fmt.Errorf("aws: failed to delete secret '%s': %v", name, err)
-		store.log(err)
+		err = fmt.Errorf("aws: failed to delete '%s': %v", key, err)
+		s.log(err)
 		return err
 	}
 	return nil
@@ -196,13 +162,13 @@ func (store *SecretsManager) Delete(name string) error {
 
 // Authenticate tries to establish a connection to
 // the AWS Secrets Manager using the login credentials.
-func (store *SecretsManager) Authenticate() error {
+func (s *SecretsManager) Authenticate() error {
 	credentials := credentials.NewStaticCredentials(
-		store.Login.AccessKey,
-		store.Login.SecretKey,
-		store.Login.SessionToken,
+		s.Login.AccessKey,
+		s.Login.SecretKey,
+		s.Login.SessionToken,
 	)
-	if store.Login.AccessKey == "" && store.Login.SecretKey == "" && store.Login.SessionToken == "" {
+	if s.Login.AccessKey == "" && s.Login.SecretKey == "" && s.Login.SessionToken == "" {
 		// If all login credentials (access key, secret key and session token) are empty
 		// we pass no (not empty) credentials to the AWS SDK. The SDK will try to fetch
 		// the credentials from:
@@ -217,8 +183,8 @@ func (store *SecretsManager) Authenticate() error {
 
 	session, err := session.NewSessionWithOptions(session.Options{
 		Config: aws.Config{
-			Endpoint:    aws.String(store.Addr),
-			Region:      aws.String(store.Region),
+			Endpoint:    aws.String(s.Addr),
+			Region:      aws.String(s.Region),
 			Credentials: credentials,
 		},
 		SharedConfigState: session.SharedConfigDisable,
@@ -226,7 +192,7 @@ func (store *SecretsManager) Authenticate() error {
 	if err != nil {
 		return err
 	}
-	store.client = secretsmanager.New(session)
+	s.client = secretsmanager.New(session)
 	return nil
 }
 
@@ -239,25 +205,10 @@ func (store *SecretsManager) Authenticate() error {
 // hasn't been called.
 var errNoConnection = errors.New("aws: no connection to AWS secrets manager")
 
-func (store *SecretsManager) initialize() {
-	if atomic.CompareAndSwapUint32(&store.once, 0, 1) {
-		store.cache.StartGC(context.Background(), store.CacheExpireAfter)
-		store.cache.StartUnusedGC(context.Background(), store.CacheExpireUnusedAfter/2)
-	}
-}
-
-func (store *SecretsManager) log(v ...interface{}) {
-	if store.ErrorLog == nil {
+func (s *SecretsManager) log(v ...interface{}) {
+	if s.ErrorLog == nil {
 		log.Println(v...)
 	} else {
-		store.ErrorLog.Println(v...)
-	}
-}
-
-func (store *SecretsManager) logf(format string, v ...interface{}) {
-	if store.ErrorLog == nil {
-		log.Printf(format, v...)
-	} else {
-		store.ErrorLog.Printf(format, v...)
+		s.ErrorLog.Println(v...)
 	}
 }

commit 68cbed539200902b93d15bb0684625699c8d085d

aws-kms: honor error log configuration
This commit fixes a bug when initializing AWS-KMS.
Before, the error log was not passed to the AWS-KMS
client such that if the client would log a error message
it would always be written to the STDOUT/STDERR - regardless
what was specified in the config file.

However, this bug does not affect any (official) release
because it has been introduced by 817a824.

cmd/kes/server.go
@@ -183,6 +183,7 @@ func server(args []string) error {
 				SecretKey:    config.KMS.AWS.Login.SecretKey,
 				SessionToken: config.KMS.AWS.Login.SessionToken,
 			},
+			ErrorLog: errorLog.Log(),
 		}
 		if err = kms.Authenticate(); err != nil {
 			return fmt.Errorf("Failed to connect to AWS-KMS: %v", err)