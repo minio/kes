2afb8c3cab4d55ddde42c7bd47f108059c803eab
Harshavardhana <harsha@minio.io>
2021-01-14 02:46:57 -0800

    do not update once updated to latest release
    avoid repeatedly updating `kes` if already on
    the latest version
    
    ```
    kes -u
    ```

--- cmd/kes/update.go
+++ cmd/kes/update.go
@@ -80,6 +80,11 @@ func updateInplace() error {
 		return err
 	}
 
+	if rel == version {
+		fmt.Printf("You are already running the most recent version of 'kes'.\n")
+		return nil
+	}
+
 	kesBin := fmt.Sprintf("https://github.com/minio/kes/releases/download/%s/kes-%s-%s", rel, runtime.GOOS, runtime.GOARCH)
 	reader, length, err := getUpdateReaderFromURL(kesBin, transport)
 	if err != nil {
