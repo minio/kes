minio/kes

commit a229d68fd98192ab4df32c296ab5febec172f5b8

config: restructure server initialization and configuration
This commit restructures the server initialization code.
Therefore, configuration-related code (types, parsing,
(de)serialization) has been moved to an internal `yml`
package.

Further, this commit improves the AWS SecretsManager
and Hashicorp Vault implementation to be self-contained
and don't depend on default configuration values.

The goal of this commit is to decouple and stabilize
internal APIs - i.e. for a stateful KES server implementation.

Signed-off-by: Andreas Auernhammer <hi@aead.dev>

cmd/kes/server.go
@@ -32,6 +32,7 @@ import (
 	"github.com/minio/kes/internal/key"
 	xlog "github.com/minio/kes/internal/log"
 	"github.com/minio/kes/internal/metric"
+	"github.com/minio/kes/internal/yml"
 	"github.com/secure-io/sio-go/sioutil"
 	"golang.org/x/crypto/ssh/terminal"
 )
@@ -103,24 +104,6 @@ func server(args []string) {
 	var ctx, cancelCtx = signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancelCtx()
 
-	config, err := loadServerConfig(configFlag)
-	if err != nil {
-		stdlog.Fatalf("Error: failed to read config file: %v", err)
-	}
-	config.SetDefaults()
-	if config.Addr == "" {
-		config.Addr = addrFlag
-	}
-	if config.TLS.KeyPath == "" {
-		config.TLS.KeyPath = tlsKeyFlag
-	}
-	if config.TLS.CertPath == "" {
-		config.TLS.CertPath = tlsCertFlag
-	}
-	if err = config.Verify(); err != nil {
-		stdlog.Fatalf("Error: %v", err)
-	}
-
 	if mlockFlag {
 		if runtime.GOOS != "linux" {
 			stdlog.Fatal("Error: cannot lock memory: syscall requires a linux system")
@@ -130,8 +113,31 @@ func server(args []string) {
 		}
 	}
 
+	config, err := yml.ReadServerConfig(configFlag)
+	if err != nil {
+		stdlog.Fatalf("Error: failed to read config file: %v", err)
+	}
+	if config.Address.Value() == "" {
+		config.Address.Set(addrFlag)
+	}
+	if config.TLS.PrivateKey.Value() == "" {
+		config.TLS.PrivateKey.Set(tlsKeyFlag)
+	}
+	if config.TLS.Certificate.Value() == "" {
+		config.TLS.Certificate.Set(tlsCertFlag)
+	}
+	if config.Admin.Identity.Value().IsUnknown() {
+		stdlog.Fatal("Error: no admin identity specified")
+	}
+	if config.TLS.PrivateKey.Value() == "" {
+		stdlog.Fatal("Error: no TLS private key specified")
+	}
+	if config.TLS.Certificate.Value() == "" {
+		stdlog.Fatal("Error: no TLS certificate specified")
+	}
+
 	var errorLog *xlog.Target
-	switch strings.ToLower(config.Log.Error) {
+	switch strings.ToLower(config.Log.Error.Value()) {
 	case "on":
 		if isTerm(os.Stderr) { // If STDERR is a tty - write plain logs, not JSON.
 			errorLog = xlog.NewTarget(os.Stderr)
@@ -141,37 +147,37 @@ func server(args []string) {
 	case "off":
 		errorLog = xlog.NewTarget(ioutil.Discard)
 	default:
-		stdlog.Fatalf("Error: %q is an invalid error log configuration", config.Log.Error)
+		stdlog.Fatalf("Error: %q is an invalid error log configuration", config.Log.Error.Value())
 	}
 
 	var auditLog *xlog.Target
-	switch strings.ToLower(config.Log.Audit) {
+	switch strings.ToLower(config.Log.Audit.Value()) {
 	case "on":
 		auditLog = xlog.NewTarget(os.Stdout)
 	case "off":
 		auditLog = xlog.NewTarget(ioutil.Discard)
 	default:
-		stdlog.Fatalf("Error: %q is an invalid audit log configuration", config.Log.Audit)
+		stdlog.Fatalf("Error: %q is an invalid audit log configuration", config.Log.Audit.Value())
 	}
 	auditLog.Log().SetFlags(0)
 
 	var proxy *auth.TLSProxy
 	if len(config.TLS.Proxy.Identities) != 0 {
 		proxy = &auth.TLSProxy{
-			CertHeader: http.CanonicalHeaderKey(config.TLS.Proxy.Header.ClientCert),
+			CertHeader: http.CanonicalHeaderKey(config.TLS.Proxy.Header.ClientCert.Value()),
 		}
 		if strings.ToLower(mtlsAuthFlag) != "off" {
 			proxy.VerifyOptions = new(x509.VerifyOptions)
 		}
 		for _, identity := range config.TLS.Proxy.Identities {
-			if !identity.IsUnknown() {
-				proxy.Add(identity)
+			if !identity.Value().IsUnknown() {
+				proxy.Add(identity.Value())
 			}
 		}
 	}
 
 	roles := &auth.Roles{
-		Root: config.Admin.Identity,
+		Root: config.Admin.Identity.Value(),
 	}
 	for name, policy := range config.Policies {
 		p, err := kes.NewPolicy(policy.Allow...)
@@ -184,25 +190,25 @@ func server(args []string) {
 		roles.Set(name, p)
 
 		for _, identity := range policy.Identities {
-			if proxy != nil && proxy.Is(identity) {
-				stdlog.Fatalf("Error: cannot assign policy %q to TLS proxy %q", name, identity)
+			if proxy != nil && proxy.Is(identity.Value()) {
+				stdlog.Fatalf("Error: cannot assign policy %q to TLS proxy %q", name, identity.Value())
 			}
-			if roles.IsAssigned(identity) {
-				stdlog.Fatalf("Error: cannot assign policy %q to identity %q: this identity already has a policy", name, identity)
+			if roles.IsAssigned(identity.Value()) {
+				stdlog.Fatalf("Error: cannot assign policy %q to identity %q: this identity already has a policy", name, identity.Value())
 			}
-			if !identity.IsUnknown() {
-				roles.Assign(name, identity)
+			if !identity.Value().IsUnknown() {
+				roles.Assign(name, identity.Value())
 			}
 		}
 	}
 
-	store, err := config.KeyStore.Connect(quietFlag, errorLog.Log())
+	store, err := connect(config, quietFlag, errorLog.Log())
 	if err != nil {
 		stdlog.Fatalf("Error: %v", err)
 	}
 	manager := &key.Manager{
-		CacheExpiryAny:    time.Duration(config.Cache.Expiry.Any),
-		CacheExpiryUnused: time.Duration(config.Cache.Expiry.Unused),
+		CacheExpiryAny:    config.Cache.Expiry.Any.Value(),
+		CacheExpiryUnused: config.Cache.Expiry.Unused.Value(),
 		Store:             store,
 	}
 
@@ -212,12 +218,12 @@ func server(args []string) {
 			stdlog.Fatalf("Error: failed to create key %q: %v", k.Name, err)
 		}
 
-		if err = store.Create(ctx, k.Name, key.New(bytes)); err != nil && err != kes.ErrKeyExists {
-			stdlog.Fatalf("Error: failed to create key %q: %v", k.Name, err)
+		if err = store.Create(ctx, k.Name.Value(), key.New(bytes)); err != nil && err != kes.ErrKeyExists {
+			stdlog.Fatalf("Error: failed to create key %q: %v", k.Name.Value(), err)
 		}
 	}
 
-	certificate, err := xhttp.LoadCertificate(config.TLS.CertPath, config.TLS.KeyPath, config.TLS.KeyPassword)
+	certificate, err := xhttp.LoadCertificate(config.TLS.Certificate.Value(), config.TLS.PrivateKey.Value(), config.TLS.Password.Value())
 	if err != nil {
 		stdlog.Fatalf("Error: failed to load TLS certificate: %v", err)
 	}
@@ -264,7 +270,7 @@ func server(args []string) {
 	mux.Handle("/", xhttp.Timeout(10*time.Second, metrics.Count(metrics.Latency(xhttp.AuditLog(auditLog.Log(), roles, xhttp.TLSProxy(proxy, http.NotFound))))))
 
 	server := http.Server{
-		Addr:    config.Addr,
+		Addr:    config.Address.Value(),
 		Handler: mux,
 		TLSConfig: &tls.Config{
 			MinVersion:     tls.VersionTLS12,
@@ -351,8 +357,8 @@ func server(args []string) {
 		bold   = color.New(color.Bold)
 		italic = color.New(color.Italic)
 	)
-	ip, port := serverAddr(config.Addr)
-	kmsKind, kmsEndpoint, err := config.KeyStore.Description()
+	ip, port := serverAddr(config.Address.Value())
+	kmsKind, kmsEndpoint, err := description(config)
 	if err != nil {
 		stdlog.Fatalf("Error: %v", err)
 	}
@@ -362,7 +368,7 @@ func server(args []string) {
 	quietFlag.Println(bold.Sprint(alignEndpoints(margin, interfaceIP4Addrs(), port)))
 	quietFlag.Println()
 
-	if r, err := hex.DecodeString(config.Admin.Identity.String()); err == nil && len(r) == sha256.Size {
+	if r, err := hex.DecodeString(config.Admin.Identity.Value().String()); err == nil && len(r) == sha256.Size {
 		quietFlag.Println(blue.Sprint("Admin:   "), config.Admin.Identity)
 	} else {
 		quietFlag.Println(blue.Sprint("Admin:   "), "_     [ disabled ]")

cmd/kes/config_v0.13.5.go
@@ -1,70 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0135 represents a KES server configuration up to
-// v0.13.5. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future.
-type serverConfigV0135 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]struct {
-		Paths      []string       `yaml:"paths"`
-		Identities []kes.Identity `yaml:"identities"`
-	} `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys kmsServerConfig `yaml:"keys"`
-}
-
-func (c *serverConfigV0135) Migrate() serverConfig {
-	config := serverConfig{
-		Addr:     c.Addr,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		KeyStore: c.Keys,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	config.Policies = make(map[string]policyConfig, len(c.Policies))
-	for name, policy := range c.Policies {
-		config.Policies[name] = policyConfig{
-			Allow:      policy.Paths,
-			Identities: policy.Identities,
-		}
-	}
-	return config
-}

cmd/kes/config_v0.14.0.go
@@ -1,76 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0140 represents a KES server configuration between v0.13.5
-// and v0.14.0. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future - once serverConfigV0135
-// got removed.
-type serverConfigV0140 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]struct {
-		Paths      []string       `yaml:"paths"`
-		Identities []kes.Identity `yaml:"identities"`
-	} `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys []struct {
-		Name string `yaml:"name"`
-	} `yaml:"keys"`
-
-	KeyStore kmsServerConfig `yaml:"keystore"`
-}
-
-func (c *serverConfigV0140) Migrate() serverConfig {
-	config := serverConfig{
-		Addr:     c.Addr,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		Keys:     c.Keys,
-		KeyStore: c.KeyStore,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	config.Policies = make(map[string]policyConfig, len(c.Policies))
-	for name, policy := range c.Policies {
-		config.Policies[name] = policyConfig{
-			Allow:      policy.Paths,
-			Identities: policy.Identities,
-		}
-	}
-	return config
-}

cmd/kes/config_v0.17.0.go
@@ -1,67 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0170 represents a KES server configuration between v0.14.0
-// and v0.17.0. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future - once serverConfigV0140
-// got removed.
-type serverConfigV0170 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]policyConfig `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys []struct {
-		Name string `yaml:"name"`
-	} `yaml:"keys"`
-
-	KeyStore kmsServerConfig `yaml:"keystore"`
-}
-
-func (c *serverConfigV0170) Migrate() serverConfig {
-	var config = serverConfig{
-		Addr:     c.Addr,
-		Policies: c.Policies,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		Keys:     c.Keys,
-		KeyStore: c.KeyStore,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	return config
-}

cmd/kes/tool.go
@@ -34,6 +34,7 @@ import (
 	"github.com/fatih/color"
 	"github.com/minio/kes"
 	"github.com/minio/kes/internal/fips"
+	"github.com/minio/kes/internal/yml"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -406,29 +407,21 @@ func migrate(args []string) {
 		pattern = "*"
 	}
 
-	sourceConfig, err := loadServerConfig(fromFlag)
+	sourceConfig, err := yml.ReadServerConfig(fromFlag)
 	if err != nil {
 		stdlog.Fatalf("Error: failed to read config file: %v", err)
 	}
-	sourceConfig.KeyStore.SetDefaults()
-	if err := sourceConfig.KeyStore.Verify(); err != nil {
-		stdlog.Fatalf("Error: %v", err)
-	}
 
-	targetConfig, err := loadServerConfig(toFlag)
+	targetConfig, err := yml.ReadServerConfig(toFlag)
 	if err != nil {
 		stdlog.Fatalf("Error: failed to read config file: %v", err)
 	}
-	targetConfig.KeyStore.SetDefaults()
-	if err := targetConfig.KeyStore.Verify(); err != nil {
-		stdlog.Fatalf("Error: %v", err)
-	}
 
-	src, err := sourceConfig.KeyStore.Connect(quietFlag, nil)
+	src, err := connect(sourceConfig, quietFlag, nil)
 	if err != nil {
 		stdlog.Fatalf("Error: %v", err)
 	}
-	dst, err := targetConfig.KeyStore.Connect(quietFlag, nil)
+	dst, err := connect(targetConfig, quietFlag, nil)
 	if err != nil {
 		stdlog.Fatalf("Error: %v", err)
 	}

internal/gcp/config.go
@@ -1,0 +1,92 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package gcp
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+)
+
+// Credentials represent GCP service account credentials.
+type Credentials struct {
+	projectID string // not exported - set by the SecretManager
+
+	// ClientID is the client ID of the GCP service account.
+	ClientID string
+
+	// Client is the client email of the GCP service account.
+	Client string
+
+	// Key is the private key ID of the GCP service account.
+	KeyID string
+
+	// Key is the encoded private key of the GCP service account.
+	Key string
+}
+
+// MarshalJSON returns a JSON representation of the GCP credentials.
+//
+// The returned JSON contains extra fields to match the JSON credentials
+// returned by GCP. Those additional fields are set to default values.
+func (c Credentials) MarshalJSON() ([]byte, error) {
+	type CredentialsJSON struct {
+		Type         string `json:"type"`
+		ProjectID    string `json:"project_id"`
+		PrivateKeyID string `json:"private_key_id"`
+		PrivateKey   string `json:"private_key"`
+		ClientEmail  string `json:"client_email"`
+		ClientID     string `json:"client_id"`
+
+		AuthURI             string `json:"auth_uri"`
+		TokenURI            string `json:"token_uri"`
+		AuthProviderCertURL string `json:"auth_provider_x509_cert_url"`
+		ClientCertURL       string `json:"client_x509_cert_url"`
+	}
+	return json.Marshal(CredentialsJSON{
+		Type:                "service_account",
+		ProjectID:           c.projectID,
+		PrivateKeyID:        c.KeyID,
+		PrivateKey:          c.Key,
+		ClientEmail:         c.Client,
+		ClientID:            c.ClientID,
+		AuthURI:             "https://accounts.google.com/o/oauth2/auth",
+		TokenURI:            "https://accounts.google.com/o/oauth2/token",
+		AuthProviderCertURL: "https://www.googleapis.com/oauth2/v1/certs",
+		ClientCertURL:       "https://www.googleapis.com/robot/v1/metadata/x509/service-account-email",
+	})
+}
+
+type Config struct {
+	Endpoint string
+
+	ProjectID string
+
+	Credentials Credentials
+
+	ErrorLog *log.Logger
+
+	lock sync.RWMutex
+}
+
+func (c *Config) Clone() *Config {
+	if c == nil {
+		return nil
+	}
+
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return &Config{
+		Endpoint:  c.Endpoint,
+		ProjectID: c.ProjectID,
+		ErrorLog:  c.ErrorLog,
+	}
+}
+
+func (c *Config) setDefaults() {
+	if c.Endpoint == "" {
+		c.Endpoint = "secretmanager.googleapis.com:443"
+	}
+}

internal/gcp/iterator.go
@@ -1,0 +1,41 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package gcp
+
+import (
+	"net/http"
+
+	"github.com/minio/kes"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	gcpiterator "google.golang.org/api/iterator"
+)
+
+var errListKey = kes.NewError(http.StatusBadGateway, "bad gateway: failed to list keys")
+
+type iterator struct {
+	src        *secretmanager.SecretIterator
+	errHandler func(error)
+	last       string
+	err        error
+}
+
+func (i *iterator) Next() bool {
+	v, err := i.src.Next()
+	if err == gcpiterator.Done {
+		return false
+	}
+	if err != nil {
+		i.errHandler(err)
+		i.err = errListKey
+		return false
+	}
+	i.last = v.GetName()
+	return true
+}
+
+func (i *iterator) Name() string { return i.last }
+
+func (i *iterator) Err() error { return i.err }

commit e8093ee884db81177b820140c7245ff1b086e86a

fix printed admin identity on startup
This commit fixes the server startup
message that displays the admin identity.

It restores the previous behavior of printing
the identity value. Currently, the server
prints the identity value twice.

This bug has been introduced by a229d68fd.

Signed-off-by: Andreas Auernhammer <hi@aead.dev>

cmd/kes/server.go
@@ -342,7 +342,7 @@ func server(args []string) {
 	quietFlag.Println()
 
 	if r, err := hex.DecodeString(config.Admin.Identity.Value().String()); err == nil && len(r) == sha256.Size {
-		quietFlag.Println(blue.Sprint("Admin:   "), config.Admin.Identity)
+		quietFlag.Println(blue.Sprint("Admin:   "), config.Admin.Identity.Value())
 	} else {
 		quietFlag.Println(blue.Sprint("Admin:   "), "_     [ disabled ]")
 	}