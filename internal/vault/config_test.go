minio/kes

commit a229d68fd98192ab4df32c296ab5febec172f5b8

config: restructure server initialization and configuration
This commit restructures the server initialization code.
Therefore, configuration-related code (types, parsing,
(de)serialization) has been moved to an internal `yml`
package.

Further, this commit improves the AWS SecretsManager
and Hashicorp Vault implementation to be self-contained
and don't depend on default configuration values.

The goal of this commit is to decouple and stabilize
internal APIs - i.e. for a stateful KES server implementation.

Signed-off-by: Andreas Auernhammer <hi@aead.dev>

internal/vault/config.go
@@ -1,0 +1,197 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package vault
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+const (
+	APIv1 = "v1"
+	APIv2 = "v2"
+)
+
+const (
+	// EngineKV is the Hashicorp Vault default KV secret engine path.
+	EngineKV = "kv"
+
+	// EngineAppRole is the Hashicorp Vault default AppRole authentication
+	// engine path.
+	EngineAppRole = "approle"
+
+	// EngineKubernetes is the Hashicorp Vault default Kubernetes
+	// authentication engine path.
+	EngineKubernetes = "kubernetes"
+)
+
+// AppRole contains authentication information
+// for the Hashicorp Vault AppRole authentication
+// API.
+//
+// Ref: https://www.vaultproject.io/api/auth/approle
+type AppRole struct {
+	// Engine is the authentication engine path
+	//
+	// Hashicorp Vault allows multiple engines of the
+	// same type mounted at the same time and/or engines
+	// mounted at arbitrary paths.
+	Engine string
+
+	// ID is the AppRole authentication ID
+	ID string
+
+	// Secret is the AppRole authentication secret.
+	Secret string
+
+	// Retry is the duration after which another
+	// authentication attempt is performed once
+	// an authentication attempt failed.
+	Retry time.Duration
+}
+
+// Kubernetes contains authentication information
+// for the Hashicorp Vault Kubernetes authentication
+// API.
+//
+// Ref: https://www.vaultproject.io/api/auth/kubernetes
+type Kubernetes struct {
+	// Engine is the authentication engine path
+	//
+	// Hashicorp Vault allows multiple engines of the
+	// same type mounted at the same time and/or engines
+	// mounted at arbitrary paths.
+	Engine string
+
+	// Role is the JWT role.
+	Role string
+
+	// JWT is the issued authentication token.
+	JWT string
+
+	// Retry is the duration after which another
+	// authentication attempt is performed once
+	// an authentication attempt failed.
+	Retry time.Duration
+}
+
+// A Config structure is used to configure a Hashicorp Vault
+// client.
+type Config struct {
+	// Endpoint is the HTTP Vault server endpoint
+	Endpoint string
+
+	// Engine is the path of the K/V engine to use.
+	//
+	// Vault allows multiple engines of the same type
+	// mounted at the same time and/or engines mounted
+	// at arbitrary paths.
+	Engine string
+
+	// APIVersion is the API version of the K/V engine.
+	//
+	// If empty, it defaults to APIv1.
+	//
+	// Ref: https://www.vaultproject.io/docs/secrets/kv
+	APIVersion string
+
+	// The Vault namespace used to separate and isolate different
+	// organizations / tenants at the same Vault instance. If
+	// non-empty, the Vault client will send the
+	//   X-Vault-Namespace: Namespace
+	// HTTP header on each request.
+	//
+	// Ref: https://www.vaultproject.io/docs/enterprise/namespaces/index.html
+	Namespace string
+
+	// Prefix is the key prefix on Vault's K/V store
+	// similar to a directory. Keys will be fetched
+	// from and stored within this prefix.
+	Prefix string
+
+	// AppRole contains the Vault AppRole authentication
+	// credentials.
+	AppRole AppRole
+
+	// K8S contains the Vault Kubernetes authentication
+	// credentials.
+	K8S Kubernetes
+
+	// ErrorLog is an optional logger for errors that
+	// may occur when interacting with an Hashicorp
+	// Vault server.
+	ErrorLog *log.Logger
+
+	// StatusPingAfter is the duration after which
+	// the KeyStore will check the status of the Vault
+	// server. Particularly, this status information
+	// is used to determine whether the Vault server
+	// has been sealed resp. unsealed again.
+	StatusPingAfter time.Duration
+
+	// Path to the mTLS client private key to authenticate to
+	// the Vault server.
+	ClientKeyPath string
+
+	// Path to the mTLS client certificate to authenticate to
+	// the Vault server.
+	ClientCertPath string
+
+	// Path to the root CA certificate(s) used to verify the
+	// TLS certificate of the Vault server. If empty, the
+	// host's root CA set is used.
+	CAPath string
+
+	lock sync.RWMutex
+}
+
+// Clone returns a shallow clone of c or nil if c is
+// nil. It is safe to clone a Config that is being used
+// concurrently.
+func (c *Config) Clone() *Config {
+	if c == nil {
+		return nil
+	}
+
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return &Config{
+		Endpoint:        c.Endpoint,
+		Engine:          c.Engine,
+		APIVersion:      c.APIVersion,
+		Namespace:       c.Namespace,
+		AppRole:         c.AppRole,
+		K8S:             c.K8S,
+		ErrorLog:        c.ErrorLog,
+		StatusPingAfter: c.StatusPingAfter,
+		ClientKeyPath:   c.ClientKeyPath,
+		ClientCertPath:  c.ClientCertPath,
+		CAPath:          c.CAPath,
+	}
+}
+
+// setDefaults overwrites some empty fields with
+// default values.
+func (c *Config) setDefaults() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	if c.Engine == "" {
+		c.Engine = EngineKV
+	}
+	if c.APIVersion == "" {
+		c.APIVersion = APIv1
+	}
+	if c.AppRole.Retry == 0 {
+		c.AppRole.Retry = 5 * time.Second
+	}
+	if c.K8S.Engine == "" {
+		c.K8S.Engine = EngineKubernetes
+	}
+	if c.K8S.Retry == 0 {
+		c.K8S.Retry = 5 * time.Second
+	}
+}

internal/gcp/config.go
@@ -1,0 +1,92 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package gcp
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+)
+
+// Credentials represent GCP service account credentials.
+type Credentials struct {
+	projectID string // not exported - set by the SecretManager
+
+	// ClientID is the client ID of the GCP service account.
+	ClientID string
+
+	// Client is the client email of the GCP service account.
+	Client string
+
+	// Key is the private key ID of the GCP service account.
+	KeyID string
+
+	// Key is the encoded private key of the GCP service account.
+	Key string
+}
+
+// MarshalJSON returns a JSON representation of the GCP credentials.
+//
+// The returned JSON contains extra fields to match the JSON credentials
+// returned by GCP. Those additional fields are set to default values.
+func (c Credentials) MarshalJSON() ([]byte, error) {
+	type CredentialsJSON struct {
+		Type         string `json:"type"`
+		ProjectID    string `json:"project_id"`
+		PrivateKeyID string `json:"private_key_id"`
+		PrivateKey   string `json:"private_key"`
+		ClientEmail  string `json:"client_email"`
+		ClientID     string `json:"client_id"`
+
+		AuthURI             string `json:"auth_uri"`
+		TokenURI            string `json:"token_uri"`
+		AuthProviderCertURL string `json:"auth_provider_x509_cert_url"`
+		ClientCertURL       string `json:"client_x509_cert_url"`
+	}
+	return json.Marshal(CredentialsJSON{
+		Type:                "service_account",
+		ProjectID:           c.projectID,
+		PrivateKeyID:        c.KeyID,
+		PrivateKey:          c.Key,
+		ClientEmail:         c.Client,
+		ClientID:            c.ClientID,
+		AuthURI:             "https://accounts.google.com/o/oauth2/auth",
+		TokenURI:            "https://accounts.google.com/o/oauth2/token",
+		AuthProviderCertURL: "https://www.googleapis.com/oauth2/v1/certs",
+		ClientCertURL:       "https://www.googleapis.com/robot/v1/metadata/x509/service-account-email",
+	})
+}
+
+type Config struct {
+	Endpoint string
+
+	ProjectID string
+
+	Credentials Credentials
+
+	ErrorLog *log.Logger
+
+	lock sync.RWMutex
+}
+
+func (c *Config) Clone() *Config {
+	if c == nil {
+		return nil
+	}
+
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+	return &Config{
+		Endpoint:  c.Endpoint,
+		ProjectID: c.ProjectID,
+		ErrorLog:  c.ErrorLog,
+	}
+}
+
+func (c *Config) setDefaults() {
+	if c.Endpoint == "" {
+		c.Endpoint = "secretmanager.googleapis.com:443"
+	}
+}

internal/yml/config.go
@@ -1,0 +1,174 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package yml
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/minio/kes"
+	"gopkg.in/yaml.v2"
+)
+
+// ReadServerConfig reads file named by filename and returns
+// the deserialized ServerConfig.
+func ReadServerConfig(filename string) (*ServerConfig, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	c, err := unmarshalServerConfig(b)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set default values for empty fields
+	if c.Cache.Expiry.Any.Value() == 0 {
+		c.Cache.Expiry.Any.value = 5 * time.Minute
+	}
+	if c.Cache.Expiry.Unused.Value() == 0 {
+		c.Cache.Expiry.Unused.value = 30 * time.Second
+	}
+	if c.Log.Audit.Value() == "" {
+		c.Log.Audit.value = "off"
+	}
+	if c.Log.Error.Value() == "" {
+		c.Log.Error.value = "on"
+	}
+	if c.KeyStore.Vault.Engine.Value() == "" {
+		c.KeyStore.Vault.Engine.value = "kv"
+	}
+	if c.KeyStore.Vault.APIVersion.Value() == "" {
+		c.KeyStore.Vault.APIVersion.value = "v1"
+	}
+	if c.KeyStore.Vault.AppRole.Engine.Value() == "" {
+		c.KeyStore.Vault.AppRole.Engine.value = "approle"
+	}
+	if c.KeyStore.Vault.AppRole.Retry.Value() == 0 {
+		c.KeyStore.Vault.AppRole.Retry.value = 5 * time.Second
+	}
+	if c.KeyStore.Vault.Kubernetes.Engine.Value() == "" {
+		c.KeyStore.Vault.Kubernetes.Engine.value = "kubernetes"
+	}
+	if c.KeyStore.Vault.Kubernetes.Retry.Value() == 0 {
+		c.KeyStore.Vault.AppRole.Retry.value = 5 * time.Second
+	}
+	if c.KeyStore.GCP.SecretManager.Endpoint.Value() == "" {
+		c.KeyStore.GCP.SecretManager.Endpoint.value = "secretmanager.googleapis.com:443"
+	}
+
+	// We treat the Hashicorp Vault Kubernetes JWT specially since it
+	// can either be the raw JWT or a path to file containing the JWT.
+	//
+	// Therefore, we check whether such a file exists, and if so, replace
+	// the JWT value with its content.
+	if c.KeyStore.Vault.Endpoint.Value() != "" && c.KeyStore.Vault.Kubernetes.JWT.Value() != "" {
+		f, err := os.Open(c.KeyStore.Vault.Kubernetes.JWT.Value())
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			return nil, fmt.Errorf("yml: failed to open Vault Kubernetes JWT: %v", err)
+		}
+		if err == nil {
+			jwt, err := ioutil.ReadAll(f)
+			if err != nil {
+				return nil, fmt.Errorf("yml: failed to read Vault Kubernetes JWT: %v", err)
+			}
+			c.KeyStore.Vault.Kubernetes.JWT.value = string(jwt)
+		}
+	}
+
+	// Validate the ServerConfig and report obvious configuration
+	// errors.
+	for _, identity := range c.TLS.Proxy.Identities {
+		if identity.Value().IsUnknown() {
+			continue
+		}
+		if identity.Value() == c.Admin.Identity.Value() {
+			return nil, errors.New("yml: TLS proxy contains admin identity: admin identity cannot be used as TLS proxy")
+		}
+	}
+
+	if v := strings.ToLower(c.Log.Audit.Value()); v != "on" && v != "off" {
+		return nil, errors.New("yml: invalid audit log configuration: allowed values are { on | off }")
+	}
+	if v := strings.ToLower(c.Log.Error.Value()); v != "on" && v != "off" {
+		return nil, errors.New("yml: invalid error log configuration: allowed values are { on | off }")
+	}
+
+	var identitySet = map[kes.Identity]string{}
+	for name, policy := range c.Policies {
+		for _, identity := range policy.Identities {
+			if identity.Value().IsUnknown() {
+				continue
+			}
+			if identity.Value() == c.Admin.Identity.Value() {
+				return nil, fmt.Errorf("yml: policy %q contains admin identity", name)
+			}
+			for _, proxyIdentity := range c.TLS.Proxy.Identities {
+				if identity.Value() == proxyIdentity.Value() {
+					return nil, fmt.Errorf("yml: policy %q contains TLS proxy identity", name)
+				}
+			}
+			if policyName, ok := identitySet[identity.Value()]; ok {
+				return nil, fmt.Errorf("yml: identity %q assigned to multiple policies: %q and %q", identity.Value(), name, policyName)
+			}
+			identitySet[identity.Value()] = name
+		}
+	}
+
+	var a backend
+	for _, b := range c.backends() {
+		if a.Endpoint != "" && b.Endpoint != "" {
+			return nil, fmt.Errorf("yml: ambiguous KMS configuration: %s and %s KMS key store specified at the same time", a.Type, b.Type)
+		}
+		if b.Endpoint != "" {
+			a = b
+		}
+	}
+
+	if c.KeyStore.Vault.Endpoint.Value() != "" {
+		if c.KeyStore.Vault.AppRole.ID.Value() != "" || c.KeyStore.Vault.AppRole.Secret.Value() != "" {
+			if c.KeyStore.Vault.Kubernetes.Role.Value() != "" || c.KeyStore.Vault.Kubernetes.JWT.Value() != "" {
+				return nil, errors.New("yml: amiguous KMS configuration: Hashicorp Vault AppRole and Kubernetes credentials found")
+			}
+		}
+	}
+	return c, nil
+}
+
+func unmarshalServerConfig(b []byte) (*ServerConfig, error) {
+	var config ServerConfig
+	if err := yaml.Unmarshal(b, &config); err != nil {
+		if _, ok := err.(*yaml.TypeError); !ok {
+			return nil, err
+		}
+
+		var configV0170 serverConfigV0170
+		if errV0170 := yaml.Unmarshal(b, &configV0170); errV0170 != nil {
+			if _, ok := errV0170.(*yaml.TypeError); !ok {
+				return nil, err
+			}
+
+			var configV0140 serverConfigV0140
+			if errV0140 := yaml.Unmarshal(b, &configV0140); errV0140 != nil {
+				if _, ok := errV0140.(*yaml.TypeError); !ok {
+					return nil, err
+				}
+
+				var configV0135 serverConfigV0135
+				if errV0135 := yaml.Unmarshal(b, &configV0135); errV0135 != nil {
+					return nil, err
+				}
+				return configV0135.migrate(), nil
+			}
+			return configV0140.migrate(), nil
+		}
+		return configV0170.migrate(), nil
+	}
+	return &config, nil
+}

cmd/kes/config_v0.13.5.go
@@ -1,70 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0135 represents a KES server configuration up to
-// v0.13.5. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future.
-type serverConfigV0135 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]struct {
-		Paths      []string       `yaml:"paths"`
-		Identities []kes.Identity `yaml:"identities"`
-	} `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys kmsServerConfig `yaml:"keys"`
-}
-
-func (c *serverConfigV0135) Migrate() serverConfig {
-	config := serverConfig{
-		Addr:     c.Addr,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		KeyStore: c.Keys,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	config.Policies = make(map[string]policyConfig, len(c.Policies))
-	for name, policy := range c.Policies {
-		config.Policies[name] = policyConfig{
-			Allow:      policy.Paths,
-			Identities: policy.Identities,
-		}
-	}
-	return config
-}

cmd/kes/config_v0.14.0.go
@@ -1,76 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0140 represents a KES server configuration between v0.13.5
-// and v0.14.0. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future - once serverConfigV0135
-// got removed.
-type serverConfigV0140 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]struct {
-		Paths      []string       `yaml:"paths"`
-		Identities []kes.Identity `yaml:"identities"`
-	} `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys []struct {
-		Name string `yaml:"name"`
-	} `yaml:"keys"`
-
-	KeyStore kmsServerConfig `yaml:"keystore"`
-}
-
-func (c *serverConfigV0140) Migrate() serverConfig {
-	config := serverConfig{
-		Addr:     c.Addr,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		Keys:     c.Keys,
-		KeyStore: c.KeyStore,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	config.Policies = make(map[string]policyConfig, len(c.Policies))
-	for name, policy := range c.Policies {
-		config.Policies[name] = policyConfig{
-			Allow:      policy.Paths,
-			Identities: policy.Identities,
-		}
-	}
-	return config
-}

cmd/kes/config_v0.17.0.go
@@ -1,67 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0170 represents a KES server configuration between v0.14.0
-// and v0.17.0. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future - once serverConfigV0140
-// got removed.
-type serverConfigV0170 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]policyConfig `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys []struct {
-		Name string `yaml:"name"`
-	} `yaml:"keys"`
-
-	KeyStore kmsServerConfig `yaml:"keystore"`
-}
-
-func (c *serverConfigV0170) Migrate() serverConfig {
-	var config = serverConfig{
-		Addr:     c.Addr,
-		Policies: c.Policies,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		Keys:     c.Keys,
-		KeyStore: c.KeyStore,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	return config
-}

cmd/kes/server.go
@@ -32,6 +32,7 @@ import (
 	"github.com/minio/kes/internal/key"
 	xlog "github.com/minio/kes/internal/log"
 	"github.com/minio/kes/internal/metric"
+	"github.com/minio/kes/internal/yml"
 	"github.com/secure-io/sio-go/sioutil"
 	"golang.org/x/crypto/ssh/terminal"
 )
@@ -103,24 +104,6 @@ func server(args []string) {
 	var ctx, cancelCtx = signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancelCtx()
 
-	config, err := loadServerConfig(configFlag)
-	if err != nil {
-		stdlog.Fatalf("Error: failed to read config file: %v", err)
-	}
-	config.SetDefaults()
-	if config.Addr == "" {
-		config.Addr = addrFlag
-	}
-	if config.TLS.KeyPath == "" {
-		config.TLS.KeyPath = tlsKeyFlag
-	}
-	if config.TLS.CertPath == "" {
-		config.TLS.CertPath = tlsCertFlag
-	}
-	if err = config.Verify(); err != nil {
-		stdlog.Fatalf("Error: %v", err)
-	}
-
 	if mlockFlag {
 		if runtime.GOOS != "linux" {
 			stdlog.Fatal("Error: cannot lock memory: syscall requires a linux system")
@@ -130,8 +113,31 @@ func server(args []string) {
 		}
 	}
 
+	config, err := yml.ReadServerConfig(configFlag)
+	if err != nil {
+		stdlog.Fatalf("Error: failed to read config file: %v", err)
+	}
+	if config.Address.Value() == "" {
+		config.Address.Set(addrFlag)
+	}
+	if config.TLS.PrivateKey.Value() == "" {
+		config.TLS.PrivateKey.Set(tlsKeyFlag)
+	}
+	if config.TLS.Certificate.Value() == "" {
+		config.TLS.Certificate.Set(tlsCertFlag)
+	}
+	if config.Admin.Identity.Value().IsUnknown() {
+		stdlog.Fatal("Error: no admin identity specified")
+	}
+	if config.TLS.PrivateKey.Value() == "" {
+		stdlog.Fatal("Error: no TLS private key specified")
+	}
+	if config.TLS.Certificate.Value() == "" {
+		stdlog.Fatal("Error: no TLS certificate specified")
+	}
+
 	var errorLog *xlog.Target
-	switch strings.ToLower(config.Log.Error) {
+	switch strings.ToLower(config.Log.Error.Value()) {
 	case "on":
 		if isTerm(os.Stderr) { // If STDERR is a tty - write plain logs, not JSON.
 			errorLog = xlog.NewTarget(os.Stderr)
@@ -141,37 +147,37 @@ func server(args []string) {
 	case "off":
 		errorLog = xlog.NewTarget(ioutil.Discard)
 	default:
-		stdlog.Fatalf("Error: %q is an invalid error log configuration", config.Log.Error)
+		stdlog.Fatalf("Error: %q is an invalid error log configuration", config.Log.Error.Value())
 	}
 
 	var auditLog *xlog.Target
-	switch strings.ToLower(config.Log.Audit) {
+	switch strings.ToLower(config.Log.Audit.Value()) {
 	case "on":
 		auditLog = xlog.NewTarget(os.Stdout)
 	case "off":
 		auditLog = xlog.NewTarget(ioutil.Discard)
 	default:
-		stdlog.Fatalf("Error: %q is an invalid audit log configuration", config.Log.Audit)
+		stdlog.Fatalf("Error: %q is an invalid audit log configuration", config.Log.Audit.Value())
 	}
 	auditLog.Log().SetFlags(0)
 
 	var proxy *auth.TLSProxy
 	if len(config.TLS.Proxy.Identities) != 0 {
 		proxy = &auth.TLSProxy{
-			CertHeader: http.CanonicalHeaderKey(config.TLS.Proxy.Header.ClientCert),
+			CertHeader: http.CanonicalHeaderKey(config.TLS.Proxy.Header.ClientCert.Value()),
 		}
 		if strings.ToLower(mtlsAuthFlag) != "off" {
 			proxy.VerifyOptions = new(x509.VerifyOptions)
 		}
 		for _, identity := range config.TLS.Proxy.Identities {
-			if !identity.IsUnknown() {
-				proxy.Add(identity)
+			if !identity.Value().IsUnknown() {
+				proxy.Add(identity.Value())
 			}
 		}
 	}
 
 	roles := &auth.Roles{
-		Root: config.Admin.Identity,
+		Root: config.Admin.Identity.Value(),
 	}
 	for name, policy := range config.Policies {
 		p, err := kes.NewPolicy(policy.Allow...)
@@ -184,25 +190,25 @@ func server(args []string) {
 		roles.Set(name, p)
 
 		for _, identity := range policy.Identities {
-			if proxy != nil && proxy.Is(identity) {
-				stdlog.Fatalf("Error: cannot assign policy %q to TLS proxy %q", name, identity)
+			if proxy != nil && proxy.Is(identity.Value()) {
+				stdlog.Fatalf("Error: cannot assign policy %q to TLS proxy %q", name, identity.Value())
 			}
-			if roles.IsAssigned(identity) {
-				stdlog.Fatalf("Error: cannot assign policy %q to identity %q: this identity already has a policy", name, identity)
+			if roles.IsAssigned(identity.Value()) {
+				stdlog.Fatalf("Error: cannot assign policy %q to identity %q: this identity already has a policy", name, identity.Value())
 			}
-			if !identity.IsUnknown() {
-				roles.Assign(name, identity)
+			if !identity.Value().IsUnknown() {
+				roles.Assign(name, identity.Value())
 			}
 		}
 	}
 
-	store, err := config.KeyStore.Connect(quietFlag, errorLog.Log())
+	store, err := connect(config, quietFlag, errorLog.Log())
 	if err != nil {
 		stdlog.Fatalf("Error: %v", err)
 	}
 	manager := &key.Manager{
-		CacheExpiryAny:    time.Duration(config.Cache.Expiry.Any),
-		CacheExpiryUnused: time.Duration(config.Cache.Expiry.Unused),
+		CacheExpiryAny:    config.Cache.Expiry.Any.Value(),
+		CacheExpiryUnused: config.Cache.Expiry.Unused.Value(),
 		Store:             store,
 	}
 
@@ -212,12 +218,12 @@ func server(args []string) {
 			stdlog.Fatalf("Error: failed to create key %q: %v", k.Name, err)
 		}
 
-		if err = store.Create(ctx, k.Name, key.New(bytes)); err != nil && err != kes.ErrKeyExists {
-			stdlog.Fatalf("Error: failed to create key %q: %v", k.Name, err)
+		if err = store.Create(ctx, k.Name.Value(), key.New(bytes)); err != nil && err != kes.ErrKeyExists {
+			stdlog.Fatalf("Error: failed to create key %q: %v", k.Name.Value(), err)
 		}
 	}
 
-	certificate, err := xhttp.LoadCertificate(config.TLS.CertPath, config.TLS.KeyPath, config.TLS.KeyPassword)
+	certificate, err := xhttp.LoadCertificate(config.TLS.Certificate.Value(), config.TLS.PrivateKey.Value(), config.TLS.Password.Value())
 	if err != nil {
 		stdlog.Fatalf("Error: failed to load TLS certificate: %v", err)
 	}
@@ -264,7 +270,7 @@ func server(args []string) {
 	mux.Handle("/", xhttp.Timeout(10*time.Second, metrics.Count(metrics.Latency(xhttp.AuditLog(auditLog.Log(), roles, xhttp.TLSProxy(proxy, http.NotFound))))))
 
 	server := http.Server{
-		Addr:    config.Addr,
+		Addr:    config.Address.Value(),
 		Handler: mux,
 		TLSConfig: &tls.Config{
 			MinVersion:     tls.VersionTLS12,
@@ -351,8 +357,8 @@ func server(args []string) {
 		bold   = color.New(color.Bold)
 		italic = color.New(color.Italic)
 	)
-	ip, port := serverAddr(config.Addr)
-	kmsKind, kmsEndpoint, err := config.KeyStore.Description()
+	ip, port := serverAddr(config.Address.Value())
+	kmsKind, kmsEndpoint, err := description(config)
 	if err != nil {
 		stdlog.Fatalf("Error: %v", err)
 	}
@@ -362,7 +368,7 @@ func server(args []string) {
 	quietFlag.Println(bold.Sprint(alignEndpoints(margin, interfaceIP4Addrs(), port)))
 	quietFlag.Println()
 
-	if r, err := hex.DecodeString(config.Admin.Identity.String()); err == nil && len(r) == sha256.Size {
+	if r, err := hex.DecodeString(config.Admin.Identity.Value().String()); err == nil && len(r) == sha256.Size {
 		quietFlag.Println(blue.Sprint("Admin:   "), config.Admin.Identity)
 	} else {
 		quietFlag.Println(blue.Sprint("Admin:   "), "_     [ disabled ]")

commit 4cbd529b6699beb57233c7d019b21d475da2b1c0

vault: include K/V prefix when cloning config
This commit fixes a bug introduced by a229d68
that ignores the K/V prefix when cloning the
Vault config.

This commit fixes this bug and adds a test to
ensure that a config is cloned as expected.

Signed-off-by: Andreas Auernhammer <hi@aead.dev>

internal/vault/config.go
@@ -163,6 +163,7 @@ func (c *Config) Clone() *Config {
 		Engine:          c.Engine,
 		APIVersion:      c.APIVersion,
 		Namespace:       c.Namespace,
+		Prefix:          c.Prefix,
 		AppRole:         c.AppRole,
 		K8S:             c.K8S,
 		ErrorLog:        c.ErrorLog,

internal/vault/config_test.go
@@ -1,0 +1,45 @@
+// Copyright 2022 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package vault
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+func TestCloneConfig(t *testing.T) {
+	for i, a := range cloneConfigTests {
+		if b := a.Clone(); *a != *b {
+			t.Fatalf("Test %d: cloned config does not match original", i)
+		}
+	}
+}
+
+var cloneConfigTests = []*Config{
+	&Config{
+		Endpoint:   "https://vault.cluster.local:8200",
+		Engine:     "secrets",
+		APIVersion: APIv2,
+		Namespace:  "ns-1",
+		Prefix:     "my-prefix",
+		AppRole: AppRole{
+			Engine: "auth",
+			ID:     "be7f3c83-9733-4d65-adaa-7eeb6e14e922",
+			Secret: "ba8d68af-23c4-4199-a516-e37cebdaab48",
+			Retry:  30 * time.Second,
+		},
+		K8S: Kubernetes{
+			Engine: "auth",
+			Role:   "kes",
+			JWT:    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+		},
+		ErrorLog:        log.Default(),
+		StatusPingAfter: 15 * time.Second,
+		ClientKeyPath:   "/tmp/kes/vault.key",
+		ClientCertPath:  "/tmp/kes/vault.crt",
+		CAPath:          "/tmp/kes/vautl.ca",
+	},
+}