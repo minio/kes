minio/kes

commit a229d68fd98192ab4df32c296ab5febec172f5b8

config: restructure server initialization and configuration
This commit restructures the server initialization code.
Therefore, configuration-related code (types, parsing,
(de)serialization) has been moved to an internal `yml`
package.

Further, this commit improves the AWS SecretsManager
and Hashicorp Vault implementation to be self-contained
and don't depend on default configuration values.

The goal of this commit is to decouple and stabilize
internal APIs - i.e. for a stateful KES server implementation.

Signed-off-by: Andreas Auernhammer <hi@aead.dev>

internal/yml/server-config.go
@@ -1,0 +1,190 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package yml
+
+// ServerConfig is the structure containing all
+// possible KES server configuration fields.
+//
+// It can be (de)serialized from/into YAML.
+type ServerConfig struct {
+	Address String `yaml:"address"`
+
+	Admin struct {
+		Identity Identity `yaml:"identity"`
+	} `yaml:"admin"`
+
+	TLS struct {
+		PrivateKey  String `yaml:"key"`
+		Certificate String `yaml:"cert"`
+		Password    String `yaml:"password"`
+
+		Proxy struct {
+			Identities []Identity `yaml:"identities"`
+			Header     struct {
+				ClientCert String `yaml:"cert"`
+			} `yaml:"header"`
+		} `yaml:"proxy"`
+	} `yaml:"tls"`
+
+	Policies map[string]struct {
+		Allow      []string   `yaml:"allow"` // Use 'string' type; We don't replace API allow patterns with env. vars
+		Deny       []string   `yaml:"deny"`  // Use 'string' type; We don't replace API deny patterns with env. vars
+		Identities []Identity `yaml:"identity"`
+	} `yaml:"policy"`
+
+	Cache struct {
+		Expiry struct {
+			Any    Duration `yaml:"any"`
+			Unused Duration `yaml:"unused"`
+		} `yaml:"expiry"`
+	} `yaml:"cache"`
+
+	Log struct {
+		Error String `yaml:"error"`
+		Audit String `yaml:"audit"`
+	} `yaml:"log"`
+
+	Keys []struct {
+		Name String `yaml:"name"`
+	} `yaml:"keys"`
+
+	KeyStore struct {
+		Fs struct {
+			Path String `yaml:"path"`
+		} `yaml:"fs"`
+
+		Generic struct {
+			Endpoint String `yaml:"endpoint"`
+			TLS      struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+		} `yaml:"generic"`
+
+		Vault struct {
+			Endpoint   String `yaml:"endpoint"`
+			Engine     String `yaml:"engine"`
+			APIVersion String `yaml:"version"`
+			Namespace  String `yaml:"namespace"`
+
+			Prefix String `yaml:"prefix"`
+
+			AppRole struct {
+				Engine String   `yaml:"engine"`
+				ID     String   `yaml:"id"`
+				Secret String   `yaml:"secret"`
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"approle"`
+
+			Kubernetes struct {
+				Engine String   `yaml:"engine"`
+				Role   String   `yaml:"role"`
+				JWT    String   `yaml:"jwt"` // Can be either a JWT or a path to a file containing a JWT
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"kubernetes"`
+
+			TLS struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+
+			Status struct {
+				Ping Duration `yaml:"ping"`
+			} `yaml:"status"`
+		} `yaml:"vault"`
+
+		Fortanix struct {
+			SDKMS struct {
+				Endpoint String `yaml:"endpoint"`
+				GroupID  String `yaml:"group_id"`
+
+				Login struct {
+					APIKey String `yaml:"key"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"sdkms"`
+		} `yaml:"fortanix"`
+
+		Aws struct {
+			SecretsManager struct {
+				Endpoint String `yaml:"endpoint"`
+				Region   String `yaml:"region"`
+				KmsKey   String ` yaml:"kmskey"`
+
+				Login struct {
+					AccessKey    String `yaml:"accesskey"`
+					SecretKey    String `yaml:"secretkey"`
+					SessionToken String `yaml:"token"`
+				} `yaml:"credentials"`
+			} `yaml:"secretsmanager"`
+		} `yaml:"aws"`
+
+		GCP struct {
+			SecretManager struct {
+				ProjectID   String `yaml:"project_id"`
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					Client   String `yaml:"client_email"`
+					ClientID String `yaml:"client_id"`
+					KeyID    String `yaml:"private_key_id"`
+					Key      String `yaml:"private_key"`
+				} `yaml:"credentials"`
+			} `yaml:"secretmanager"`
+		} `yaml:"gcp"`
+
+		Azure struct {
+			KeyVault struct {
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					TenantID String `yaml:"tenant_id"`
+					ClientID String `yaml:"client_id"`
+					Secret   String `yaml:"client_secret"`
+				} `yaml:"credentials"`
+				ManagedIdentity struct {
+					ClientID String `yaml:"client_id"`
+				} `yaml:"managed_identity"`
+			} `yaml:"keyvault"`
+		} `yaml:"azure"`
+
+		Gemalto struct {
+			KeySecure struct {
+				Endpoint String `yaml:"endpoint"`
+
+				Login struct {
+					Token  String   `yaml:"token"`
+					Domain String   `yaml:"domain"`
+					Retry  Duration `yaml:"retry"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"keysecure"`
+		} `yaml:"gemalto"`
+	} `yaml:"keystore"`
+}
+
+type backend struct {
+	Type     string
+	Endpoint string
+}
+
+func (c *ServerConfig) backends() []backend {
+	return []backend{
+		backend{Type: "FS", Endpoint: c.KeyStore.Fs.Path.Value()},
+		backend{Type: "Generic", Endpoint: c.KeyStore.Generic.Endpoint.Value()},
+		backend{Type: "Hashicorp Vault", Endpoint: c.KeyStore.Vault.Endpoint.Value()},
+		backend{Type: "Fortanix SDKMS", Endpoint: c.KeyStore.Fortanix.SDKMS.Endpoint.Value()},
+		backend{Type: "Gemalto KeySecure", Endpoint: c.KeyStore.Gemalto.KeySecure.Endpoint.Value()},
+		backend{Type: "AWS SecretsManager", Endpoint: c.KeyStore.Aws.SecretsManager.Endpoint.Value()},
+		backend{Type: "GCP SecretManager", Endpoint: c.KeyStore.GCP.SecretManager.ProjectID.Value()},
+		backend{Type: "Azure KeyVault", Endpoint: c.KeyStore.Azure.KeyVault.Endpoint.Value()},
+	}
+}

internal/yml/server-config_v0.13.5.go
@@ -1,0 +1,190 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package yml
+
+type serverConfigV0135 struct {
+	Addr String   `yaml:"address"`
+	Root Identity `yaml:"root"`
+
+	TLS struct {
+		PrivateKey  String `yaml:"key"`
+		Certificate String `yaml:"cert"`
+		Proxy       struct {
+			Identities []Identity `yaml:"identities"`
+			Header     struct {
+				ClientCert String `yaml:"cert"`
+			} `yaml:"header"`
+		} `yaml:"proxy"`
+	} `yaml:"tls"`
+
+	Policies map[string]struct {
+		Paths      []string   `yaml:"paths"` // Use 'string' type; We don't replace API path patterns with env. vars
+		Identities []Identity `yaml:"identities"`
+	} `yaml:"policy"`
+
+	Cache struct {
+		Expiry struct {
+			Any    Duration `yaml:"any"`
+			Unused Duration `yaml:"unused"`
+		} `yaml:"expiry"`
+	} `yaml:"cache"`
+
+	Log struct {
+		Error String `yaml:"error"`
+		Audit String `yaml:"audit"`
+	} `yaml:"log"`
+
+	Keys struct {
+		Fs struct {
+			Path String `yaml:"path"`
+		} `yaml:"fs"`
+
+		Generic struct {
+			Endpoint String `yaml:"endpoint"`
+			TLS      struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+		} `yaml:"generic"`
+
+		Vault struct {
+			Endpoint   String `yaml:"endpoint"`
+			Engine     String `yaml:"engine"`
+			APIVersion String `yaml:"version"`
+			Namespace  String `yaml:"namespace"`
+
+			Prefix String `yaml:"prefix"`
+
+			AppRole struct {
+				Engine String   `yaml:"engine"`
+				ID     String   `yaml:"id"`
+				Secret String   `yaml:"secret"`
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"approle"`
+
+			Kubernetes struct {
+				Engine String   `yaml:"engine"`
+				Role   String   `yaml:"role"`
+				JWT    String   `yaml:"jwt"` // Can be either a JWT or a path to a file containing a JWT
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"kubernetes"`
+
+			TLS struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+
+			Status struct {
+				Ping Duration `yaml:"ping"`
+			} `yaml:"status"`
+		} `yaml:"vault"`
+
+		Fortanix struct {
+			SDKMS struct {
+				Endpoint String `yaml:"endpoint"`
+				GroupID  String `yaml:"group_id"`
+
+				Login struct {
+					APIKey String `yaml:"key"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"sdkms"`
+		} `yaml:"fortanix"`
+
+		Aws struct {
+			SecretsManager struct {
+				Endpoint String `yaml:"endpoint"`
+				Region   String `yaml:"region"`
+				KmsKey   String ` yaml:"kmskey"`
+
+				Login struct {
+					AccessKey    String `yaml:"accesskey"`
+					SecretKey    String `yaml:"secretkey"`
+					SessionToken String `yaml:"token"`
+				} `yaml:"credentials"`
+			} `yaml:"secretsmanager"`
+		} `yaml:"aws"`
+
+		GCP struct {
+			SecretManager struct {
+				ProjectID   String `yaml:"project_id"`
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					Client   String `yaml:"client_email"`
+					ClientID String `yaml:"client_id"`
+					KeyID    String `yaml:"private_key_id"`
+					Key      String `yaml:"private_key"`
+				} `yaml:"credentials"`
+			} `yaml:"secretmanager"`
+		} `yaml:"gcp"`
+
+		Azure struct {
+			KeyVault struct {
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					TenantID String `yaml:"tenant_id"`
+					ClientID String `yaml:"client_id"`
+					Secret   String `yaml:"client_secret"`
+				} `yaml:"credentials"`
+				ManagedIdentity struct {
+					ClientID String `yaml:"client_id"`
+				} `yaml:"managed_identity"`
+			} `yaml:"keyvault"`
+		} `yaml:"azure"`
+
+		Gemalto struct {
+			KeySecure struct {
+				Endpoint String `yaml:"endpoint"`
+
+				Login struct {
+					Token  String   `yaml:"token"`
+					Domain String   `yaml:"domain"`
+					Retry  Duration `yaml:"retry"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"keysecure"`
+		} `yaml:"gemalto"`
+	} `yaml:"keys"`
+}
+
+func (c *serverConfigV0135) migrate() *ServerConfig {
+	var config = &ServerConfig{
+		Address:  c.Addr,
+		Cache:    c.Cache,
+		Log:      c.Log,
+		KeyStore: c.Keys,
+	}
+	config.Admin.Identity = c.Root
+
+	config.TLS.PrivateKey = c.TLS.PrivateKey
+	config.TLS.Certificate = c.TLS.Certificate
+	config.TLS.Proxy = c.TLS.Proxy
+
+	type Policy struct {
+		Allow      []string   `yaml:"allow"`
+		Deny       []string   `yaml:"deny"`
+		Identities []Identity `yaml:"identity"`
+	}
+	config.Policies = make(map[string]struct {
+		Allow      []string   `yaml:"allow"`
+		Deny       []string   `yaml:"deny"`
+		Identities []Identity `yaml:"identity"`
+	}, len(c.Policies))
+	for name, policy := range c.Policies {
+		config.Policies[name] = Policy{
+			Allow:      policy.Paths,
+			Identities: policy.Identities,
+		}
+	}
+	return config
+}

internal/yml/server-config_v0.14.0.go
@@ -1,0 +1,195 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package yml
+
+type serverConfigV0140 struct {
+	Addr String   `yaml:"address"`
+	Root Identity `yaml:"root"`
+
+	TLS struct {
+		PrivateKey  String `yaml:"key"`
+		Certificate String `yaml:"cert"`
+		Proxy       struct {
+			Identities []Identity `yaml:"identities"`
+			Header     struct {
+				ClientCert String `yaml:"cert"`
+			} `yaml:"header"`
+		} `yaml:"proxy"`
+	} `yaml:"tls"`
+
+	Policies map[string]struct {
+		Paths      []string   `yaml:"paths"` // Use 'string' type; We don't replace API path patterns with env. vars
+		Identities []Identity `yaml:"identities"`
+	} `yaml:"policy"`
+
+	Cache struct {
+		Expiry struct {
+			Any    Duration `yaml:"any"`
+			Unused Duration `yaml:"unused"`
+		} `yaml:"expiry"`
+	} `yaml:"cache"`
+
+	Log struct {
+		Error String `yaml:"error"`
+		Audit String `yaml:"audit"`
+	} `yaml:"log"`
+
+	Keys []struct {
+		Name String `yaml:"name"`
+	} `yaml:"keys"`
+
+	KeyStore struct {
+		Fs struct {
+			Path String `yaml:"path"`
+		} `yaml:"fs"`
+
+		Generic struct {
+			Endpoint String `yaml:"endpoint"`
+			TLS      struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+		} `yaml:"generic"`
+
+		Vault struct {
+			Endpoint   String `yaml:"endpoint"`
+			Engine     String `yaml:"engine"`
+			APIVersion String `yaml:"version"`
+			Namespace  String `yaml:"namespace"`
+
+			Prefix String `yaml:"prefix"`
+
+			AppRole struct {
+				Engine String   `yaml:"engine"`
+				ID     String   `yaml:"id"`
+				Secret String   `yaml:"secret"`
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"approle"`
+
+			Kubernetes struct {
+				Engine String   `yaml:"engine"`
+				Role   String   `yaml:"role"`
+				JWT    String   `yaml:"jwt"` // Can be either a JWT or a path to a file containing a JWT
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"kubernetes"`
+
+			TLS struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+
+			Status struct {
+				Ping Duration `yaml:"ping"`
+			} `yaml:"status"`
+		} `yaml:"vault"`
+
+		Fortanix struct {
+			SDKMS struct {
+				Endpoint String `yaml:"endpoint"`
+				GroupID  String `yaml:"group_id"`
+
+				Login struct {
+					APIKey String `yaml:"key"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"sdkms"`
+		} `yaml:"fortanix"`
+
+		Aws struct {
+			SecretsManager struct {
+				Endpoint String `yaml:"endpoint"`
+				Region   String `yaml:"region"`
+				KmsKey   String ` yaml:"kmskey"`
+
+				Login struct {
+					AccessKey    String `yaml:"accesskey"`
+					SecretKey    String `yaml:"secretkey"`
+					SessionToken String `yaml:"token"`
+				} `yaml:"credentials"`
+			} `yaml:"secretsmanager"`
+		} `yaml:"aws"`
+
+		GCP struct {
+			SecretManager struct {
+				ProjectID   String `yaml:"project_id"`
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					Client   String `yaml:"client_email"`
+					ClientID String `yaml:"client_id"`
+					KeyID    String `yaml:"private_key_id"`
+					Key      String `yaml:"private_key"`
+				} `yaml:"credentials"`
+			} `yaml:"secretmanager"`
+		} `yaml:"gcp"`
+
+		Azure struct {
+			KeyVault struct {
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					TenantID String `yaml:"tenant_id"`
+					ClientID String `yaml:"client_id"`
+					Secret   String `yaml:"client_secret"`
+				} `yaml:"credentials"`
+				ManagedIdentity struct {
+					ClientID String `yaml:"client_id"`
+				} `yaml:"managed_identity"`
+			} `yaml:"keyvault"`
+		} `yaml:"azure"`
+
+		Gemalto struct {
+			KeySecure struct {
+				Endpoint String `yaml:"endpoint"`
+
+				Login struct {
+					Token  String   `yaml:"token"`
+					Domain String   `yaml:"domain"`
+					Retry  Duration `yaml:"retry"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"keysecure"`
+		} `yaml:"gemalto"`
+	} `yaml:"keystore"`
+}
+
+func (c *serverConfigV0140) migrate() *ServerConfig {
+	var config = &ServerConfig{
+		Address:  c.Addr,
+		Cache:    c.Cache,
+		Log:      c.Log,
+		Keys:     c.Keys,
+		KeyStore: c.KeyStore,
+	}
+	config.Admin.Identity = c.Root
+
+	config.TLS.PrivateKey = c.TLS.PrivateKey
+	config.TLS.Certificate = c.TLS.Certificate
+	config.TLS.Proxy = c.TLS.Proxy
+
+	type Policy struct {
+		Allow      []string   `yaml:"allow"`
+		Deny       []string   `yaml:"deny"`
+		Identities []Identity `yaml:"identity"`
+	}
+	config.Policies = make(map[string]struct {
+		Allow      []string   `yaml:"allow"`
+		Deny       []string   `yaml:"deny"`
+		Identities []Identity `yaml:"identity"`
+	}, len(c.Policies))
+	for name, policy := range c.Policies {
+		config.Policies[name] = Policy{
+			Allow:      policy.Paths,
+			Identities: policy.Identities,
+		}
+	}
+	return config
+}

internal/yml/server-config_v0.17.0.go
@@ -1,0 +1,181 @@
+// Copyright 2021 - MinIO, Inc. All rights reserved.
+// Use of this source code is governed by the AGPLv3
+// license that can be found in the LICENSE file.
+
+package yml
+
+type serverConfigV0170 struct {
+	Addr String   `yaml:"address"`
+	Root Identity `yaml:"root"`
+
+	TLS struct {
+		PrivateKey  String `yaml:"key"`
+		Certificate String `yaml:"cert"`
+		Proxy       struct {
+			Identities []Identity `yaml:"identities"`
+			Header     struct {
+				ClientCert String `yaml:"cert"`
+			} `yaml:"header"`
+		} `yaml:"proxy"`
+	} `yaml:"tls"`
+
+	Policies map[string]struct {
+		Allow      []string   `yaml:"allow"` // Use 'string' type; We don't replace API allow patterns with env. vars
+		Deny       []string   `yaml:"deny"`  // Use 'string' type; We don't replace API deny patterns with env. vars
+		Identities []Identity `yaml:"identity"`
+	} `yaml:"policy"`
+
+	Cache struct {
+		Expiry struct {
+			Any    Duration `yaml:"any"`
+			Unused Duration `yaml:"unused"`
+		} `yaml:"expiry"`
+	} `yaml:"cache"`
+
+	Log struct {
+		Error String `yaml:"error"`
+		Audit String `yaml:"audit"`
+	} `yaml:"log"`
+
+	Keys []struct {
+		Name String `yaml:"name"`
+	} `yaml:"keys"`
+
+	KeyStore struct {
+		Fs struct {
+			Path String `yaml:"path"`
+		} `yaml:"fs"`
+
+		Generic struct {
+			Endpoint String `yaml:"endpoint"`
+			TLS      struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+		} `yaml:"generic"`
+
+		Vault struct {
+			Endpoint   String `yaml:"endpoint"`
+			Engine     String `yaml:"engine"`
+			APIVersion String `yaml:"version"`
+			Namespace  String `yaml:"namespace"`
+
+			Prefix String `yaml:"prefix"`
+
+			AppRole struct {
+				Engine String   `yaml:"engine"`
+				ID     String   `yaml:"id"`
+				Secret String   `yaml:"secret"`
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"approle"`
+
+			Kubernetes struct {
+				Engine String   `yaml:"engine"`
+				Role   String   `yaml:"role"`
+				JWT    String   `yaml:"jwt"` // Can be either a JWT or a path to a file containing a JWT
+				Retry  Duration `yaml:"retry"`
+			} `yaml:"kubernetes"`
+
+			TLS struct {
+				PrivateKey  String `yaml:"key"`
+				Certificate String `yaml:"cert"`
+				CAPath      String `yaml:"ca"`
+			} `yaml:"tls"`
+
+			Status struct {
+				Ping Duration `yaml:"ping"`
+			} `yaml:"status"`
+		} `yaml:"vault"`
+
+		Fortanix struct {
+			SDKMS struct {
+				Endpoint String `yaml:"endpoint"`
+				GroupID  String `yaml:"group_id"`
+
+				Login struct {
+					APIKey String `yaml:"key"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"sdkms"`
+		} `yaml:"fortanix"`
+
+		Aws struct {
+			SecretsManager struct {
+				Endpoint String `yaml:"endpoint"`
+				Region   String `yaml:"region"`
+				KmsKey   String ` yaml:"kmskey"`
+
+				Login struct {
+					AccessKey    String `yaml:"accesskey"`
+					SecretKey    String `yaml:"secretkey"`
+					SessionToken String `yaml:"token"`
+				} `yaml:"credentials"`
+			} `yaml:"secretsmanager"`
+		} `yaml:"aws"`
+
+		GCP struct {
+			SecretManager struct {
+				ProjectID   String `yaml:"project_id"`
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					Client   String `yaml:"client_email"`
+					ClientID String `yaml:"client_id"`
+					KeyID    String `yaml:"private_key_id"`
+					Key      String `yaml:"private_key"`
+				} `yaml:"credentials"`
+			} `yaml:"secretmanager"`
+		} `yaml:"gcp"`
+
+		Azure struct {
+			KeyVault struct {
+				Endpoint    String `yaml:"endpoint"`
+				Credentials struct {
+					TenantID String `yaml:"tenant_id"`
+					ClientID String `yaml:"client_id"`
+					Secret   String `yaml:"client_secret"`
+				} `yaml:"credentials"`
+				ManagedIdentity struct {
+					ClientID String `yaml:"client_id"`
+				} `yaml:"managed_identity"`
+			} `yaml:"keyvault"`
+		} `yaml:"azure"`
+
+		Gemalto struct {
+			KeySecure struct {
+				Endpoint String `yaml:"endpoint"`
+
+				Login struct {
+					Token  String   `yaml:"token"`
+					Domain String   `yaml:"domain"`
+					Retry  Duration `yaml:"retry"`
+				} `yaml:"credentials"`
+
+				TLS struct {
+					CAPath String `yaml:"ca"`
+				} `yaml:"tls"`
+			} `yaml:"keysecure"`
+		} `yaml:"gemalto"`
+	} `yaml:"keystore"`
+}
+
+func (c *serverConfigV0170) migrate() *ServerConfig {
+	var config = &ServerConfig{
+		Address:  c.Addr,
+		Policies: c.Policies,
+		Cache:    c.Cache,
+		Log:      c.Log,
+		Keys:     c.Keys,
+		KeyStore: c.KeyStore,
+	}
+	config.Admin.Identity = c.Root
+
+	config.TLS.PrivateKey = c.TLS.PrivateKey
+	config.TLS.Certificate = c.TLS.Certificate
+	config.TLS.Proxy = c.TLS.Proxy
+	return config
+
+}

cmd/kes/config_v0.13.5.go
@@ -1,70 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0135 represents a KES server configuration up to
-// v0.13.5. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future.
-type serverConfigV0135 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]struct {
-		Paths      []string       `yaml:"paths"`
-		Identities []kes.Identity `yaml:"identities"`
-	} `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys kmsServerConfig `yaml:"keys"`
-}
-
-func (c *serverConfigV0135) Migrate() serverConfig {
-	config := serverConfig{
-		Addr:     c.Addr,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		KeyStore: c.Keys,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	config.Policies = make(map[string]policyConfig, len(c.Policies))
-	for name, policy := range c.Policies {
-		config.Policies[name] = policyConfig{
-			Allow:      policy.Paths,
-			Identities: policy.Identities,
-		}
-	}
-	return config
-}

cmd/kes/config_v0.14.0.go
@@ -1,76 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0140 represents a KES server configuration between v0.13.5
-// and v0.14.0. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future - once serverConfigV0135
-// got removed.
-type serverConfigV0140 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]struct {
-		Paths      []string       `yaml:"paths"`
-		Identities []kes.Identity `yaml:"identities"`
-	} `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys []struct {
-		Name string `yaml:"name"`
-	} `yaml:"keys"`
-
-	KeyStore kmsServerConfig `yaml:"keystore"`
-}
-
-func (c *serverConfigV0140) Migrate() serverConfig {
-	config := serverConfig{
-		Addr:     c.Addr,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		Keys:     c.Keys,
-		KeyStore: c.KeyStore,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	config.Policies = make(map[string]policyConfig, len(c.Policies))
-	for name, policy := range c.Policies {
-		config.Policies[name] = policyConfig{
-			Allow:      policy.Paths,
-			Identities: policy.Identities,
-		}
-	}
-	return config
-}

cmd/kes/config_v0.17.0.go
@@ -1,67 +1,0 @@
-// Copyright 2021 - MinIO, Inc. All rights reserved.
-// Use of this source code is governed by the AGPLv3
-// license that can be found in the LICENSE file.
-
-package main
-
-import "github.com/minio/kes"
-
-// serverConfigV0170 represents a KES server configuration between v0.14.0
-// and v0.17.0. It provides backward-compatible unmarshaling of exiting
-// configuration files.
-//
-// It will be removed at some time in the future - once serverConfigV0140
-// got removed.
-type serverConfigV0170 struct {
-	Addr string       `yaml:"address"`
-	Root kes.Identity `yaml:"root"`
-
-	TLS struct {
-		KeyPath  string `yaml:"key"`
-		CertPath string `yaml:"cert"`
-		Proxy    struct {
-			Identities []kes.Identity `yaml:"identities"`
-			Header     struct {
-				ClientCert string `yaml:"cert"`
-			} `yaml:"header"`
-		} `yaml:"proxy"`
-	} `yaml:"tls"`
-
-	Policies map[string]policyConfig `yaml:"policy"`
-
-	Cache struct {
-		Expiry struct {
-			Any    duration `yaml:"any"`    // Use custom type for env. var support
-			Unused duration `yaml:"unused"` // Use custom type for env. var support
-		} `yaml:"expiry"`
-	} `yaml:"cache"`
-
-	Log struct {
-		Error string `yaml:"error"`
-		Audit string `yaml:"audit"`
-	} `yaml:"log"`
-
-	Keys []struct {
-		Name string `yaml:"name"`
-	} `yaml:"keys"`
-
-	KeyStore kmsServerConfig `yaml:"keystore"`
-}
-
-func (c *serverConfigV0170) Migrate() serverConfig {
-	var config = serverConfig{
-		Addr:     c.Addr,
-		Policies: c.Policies,
-		Cache:    c.Cache,
-		Log:      c.Log,
-		Keys:     c.Keys,
-		KeyStore: c.KeyStore,
-	}
-	config.Admin.Identity = c.Root
-
-	config.TLS.KeyPath = c.TLS.KeyPath
-	config.TLS.CertPath = c.TLS.CertPath
-	config.TLS.Proxy = c.TLS.Proxy
-
-	return config
-}

commit b881059e1dce0c8ce82a26c63df47a5095dc170e

yml: fix regression parsing policy definitions
This commit fixes a regression introduced by
a229d68.

Identities assigned to a policy in the YAML
config file have been ignored in the following
case:
```
policy:
   - my-policy:
       allow:
         - /v1/key/create/*
       identity:
         - ${MY_CLIENT_IDENTITY}
```

Before, the config file parsing expected
`identities` - not `identity`. After a229d68
`identities` has been ignored. This commit
fixes this by reverting to the previous behavior
of only honoring `identities`.

Signed-off-by: Andreas Auernhammer <hi@aead.dev>

internal/yml/server-config.go
@@ -31,7 +31,7 @@ type ServerConfig struct {
 	Policies map[string]struct {
 		Allow      []string   `yaml:"allow"` // Use 'string' type; We don't replace API allow patterns with env. vars
 		Deny       []string   `yaml:"deny"`  // Use 'string' type; We don't replace API deny patterns with env. vars
-		Identities []Identity `yaml:"identity"`
+		Identities []Identity `yaml:"identities"`
 	} `yaml:"policy"`
 
 	Cache struct {

internal/yml/server-config_v0.13.5.go
@@ -174,12 +174,12 @@ func (c *serverConfigV0135) migrate() *ServerConfig {
 	type Policy struct {
 		Allow      []string   `yaml:"allow"`
 		Deny       []string   `yaml:"deny"`
-		Identities []Identity `yaml:"identity"`
+		Identities []Identity `yaml:"identities"`
 	}
 	config.Policies = make(map[string]struct {
 		Allow      []string   `yaml:"allow"`
 		Deny       []string   `yaml:"deny"`
-		Identities []Identity `yaml:"identity"`
+		Identities []Identity `yaml:"identities"`
 	}, len(c.Policies))
 	for name, policy := range c.Policies {
 		config.Policies[name] = Policy{

internal/yml/server-config_v0.14.0.go
@@ -179,12 +179,12 @@ func (c *serverConfigV0140) migrate() *ServerConfig {
 	type Policy struct {
 		Allow      []string   `yaml:"allow"`
 		Deny       []string   `yaml:"deny"`
-		Identities []Identity `yaml:"identity"`
+		Identities []Identity `yaml:"identities"`
 	}
 	config.Policies = make(map[string]struct {
 		Allow      []string   `yaml:"allow"`
 		Deny       []string   `yaml:"deny"`
-		Identities []Identity `yaml:"identity"`
+		Identities []Identity `yaml:"identities"`
 	}, len(c.Policies))
 	for name, policy := range c.Policies {
 		config.Policies[name] = Policy{

internal/yml/server-config_v0.17.0.go
@@ -22,7 +22,7 @@ type serverConfigV0170 struct {
 	Policies map[string]struct {
 		Allow      []string   `yaml:"allow"` // Use 'string' type; We don't replace API allow patterns with env. vars
 		Deny       []string   `yaml:"deny"`  // Use 'string' type; We don't replace API deny patterns with env. vars
-		Identities []Identity `yaml:"identity"`
+		Identities []Identity `yaml:"identities"`
 	} `yaml:"policy"`
 
 	Cache struct {