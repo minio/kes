20d120a9af6cdba4623e57ab1330762654220c51
Javier <reivaj0705@gmail.com>
2023-01-09 16:20:43 -0600

    Reference to KES

--- web-app/src/common/types.ts
+++ web-app/src/common/types.ts
@@ -1,4 +1,4 @@
-// This file is part of MinIO Console Server
+// This file is part of KES
 // Copyright (c) 2023 MinIO, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
--- web-app/src/screens/console/ConsoleApp.tsx
+++ web-app/src/screens/console/ConsoleApp.tsx
@@ -1,4 +1,4 @@
-//  This file is part of MinIO Console Server
+//  This file is part of MinIO KES
 //  Copyright (c) 2023 MinIO, Inc.
 //
 //  This program is free software: you can redistribute it and/or modify
