dd6b6adb85ca0f595a3801fc08083914235eaf90
Javier <reivaj0705@gmail.com>
2023-03-15 16:28:40 -0600

    Update login info

--- web-app/src/screens/login/Login.tsx
+++ web-app/src/screens/login/Login.tsx
@@ -37,13 +37,7 @@ const Login = () => {
       promoHeader={<Fragment>KES</Fragment>}
       promoInfo={
         <Fragment>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          KES is a cloud-native distributed key management and encryption server designed to secure modern applications at scale.
         </Fragment>
       }
       logoProps={{
